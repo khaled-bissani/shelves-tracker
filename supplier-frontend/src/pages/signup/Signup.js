919fa141f98d3e7c5978f4361d00d3925ad30554
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 13:35:06 +0300

    Frontend <> Supplier frontend: adding use state for all the signup input fields

--- supplier-frontend/src/pages/signup/Signup.js
+++ supplier-frontend/src/pages/signup/Signup.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SignupLoginButton from '../../components/SignupLoginButton'
 import TextInput from '../../components/TextInput'
 import Background from '../../assets/background.jpg'
 
 const Signup = () => {
+    
+    const [user, setUser] = useState({
+        full_name: '',
+        email: '',
+        password: '',
+        current_password: ''
+    });
+
   return (
     <div className='flex justify-end h-screen' style={{backgroundImage : `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
         <div className='flex flex-col items-center justify-around w-[375px] h-[450px] bg-[#3AA346] mt-28 mr-56'>
