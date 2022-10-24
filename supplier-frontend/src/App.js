9f09c6de1ee20bc86b4a23f16da5f22c838386cc
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 13:54:26 +0300

    Frontend <> Supplier frontend: adding route to the signup page

--- supplier-frontend/src/App.js
+++ supplier-frontend/src/App.js
@@ -1,9 +1,14 @@
 import Signup from "./pages/signup/Signup";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Signup />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Signup/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
