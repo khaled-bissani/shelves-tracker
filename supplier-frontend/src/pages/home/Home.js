da5ab1a850e4de8b9849743cf85c6de250337289
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 23:21:32 +0300

    Frontend <> Supplier frontend: adding elements to the home page

--- supplier-frontend/src/pages/home/Home.js
+++ supplier-frontend/src/pages/home/Home.js
@@ -3,8 +3,19 @@ import SideBar from '../../components/SideBar'
 
 const Home = () => {
   return (
-    <div>
+    <div className='flex'>
       <SideBar/>
+      <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
+        <p className='text-[24px] font-sans font-bold'>Dashboard</p>
+        <div className='w-[900px] h-[100px] bg-[#D9D9D9]'></div>
+        <div className='flex justify-between'>
+          <div className='w-[575px] h-[300px] bg-[#D9D9D9]'></div>
+          <div className='flex flex-col justify-between'>
+            <div className='w-[300px] h-[140px] bg-[#D9D9D9]'></div>
+            <div className='w-[300px] h-[140px] bg-[#D9D9D9]'></div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
