da43d62fbcb14d62efe293c7c408a8d0f5c28976
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 14:26:41 +0300

    Frontend <> Supplier frontend: adding the use state and the handle method to each text input in the login page

--- supplier-frontend/src/pages/login/Login.js
+++ supplier-frontend/src/pages/login/Login.js
@@ -21,9 +21,9 @@ const Login = () => {
     <div className='flex justify-end h-screen' style={{backgroundImage : `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
         <div className='flex flex-col items-center justify-around w-[375px] h-[450px] bg-[#3AA346] mt-28 mr-56'>
             <h1 className='font-sans font-bold text-[24px] text-white'>Login</h1>
-            <TextInput label={"Email"} type={"email"} placeholder={"Email"} />
-            <TextInput label={"Password"} type={"password"} placeholder={"Password"} />
-            <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} />
+            <TextInput label={"Email"} type={"email"} placeholder={"Email"} value={user.email} onChange={(e)=>handleChange(e,"email")}/>
+            <TextInput label={"Password"} type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")}/>
+            <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} value={user.current_password} onChange={(e)=>handleChange(e,"current_password")}/>
             <SignupLoginButton text={"LOGIN"}/>
             <p className='font-sans'>Don't have an account?
                 <span className='text-white cursor-pointer'> Signup</span>
