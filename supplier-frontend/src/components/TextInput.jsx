b6d3be9dc3315b2e13e94a8687398f44805b5b0f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 12:06:25 +0300

    Frontend <> Supplier frontend: adding label to the input field component

--- supplier-frontend/src/components/TextInput.jsx
+++ supplier-frontend/src/components/TextInput.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-const TextInput = ({type,placeholder}) => {
+const TextInput = ({label,type,placeholder}) => {
   return (
-    <div>
-        <input className="outline-none w-[300px] h-[40px] pl-4 bg-white" type={type} placeholder={placeholder} />
+    <div className='flex flex-col'>
+        <label className='font-sans text-[16px] text-white'>{label}</label>
+        <input className="outline-none w-[300px] h-[40px] pl-4 bg-white font-sans text-[14px]" type={type} placeholder={placeholder} />
     </div>
   )
 }
--- supplier-frontend/src/pages/signup/Signup.js
+++ supplier-frontend/src/pages/signup/Signup.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import TextInput from '../../components/TextInput'
 
 const Signup = () => {
   return (
     <div>
-        
+        <TextInput label={"Name"} type={"text"} placeholder={"Name"}/>
     </div>
   )
 }
