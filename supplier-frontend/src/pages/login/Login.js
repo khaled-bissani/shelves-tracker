cad4d90b757af3ec876365080d8b0c32d4cc503b
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 14:21:29 +0300

    Frontend <> Supplier frontend: adding all the components to the login page

--- supplier-frontend/src/pages/login/Login.js
+++ supplier-frontend/src/pages/login/Login.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import SignupLoginButton from '../../components/SignupLoginButton'
+import TextInput from '../../components/TextInput'
+import Background from '../../assets/background.jpg'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='flex justify-end h-screen' style={{backgroundImage : `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
+        <div className='flex flex-col items-center justify-around w-[375px] h-[450px] bg-[#3AA346] mt-28 mr-56'>
+            <h1 className='font-sans font-bold text-[24px] text-white'>Login</h1>
+            <TextInput label={"Email"} type={"email"} placeholder={"Email"} />
+            <TextInput label={"Password"} type={"password"} placeholder={"Password"} />
+            <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} />
+            <SignupLoginButton text={"LOGIN"}/>
+            <p className='font-sans'>Don't have an account?
+                <span className='text-white cursor-pointer'> Signup</span>
+            </p>
+        </div>
+    </div>
   )
 }
 
