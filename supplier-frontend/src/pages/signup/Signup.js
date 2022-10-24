0b8efb90cef7c2ab86ba11ef0676f375c9ceb9ac
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 14:21:29 +0300

    Frontend <> Supplier frontend: adding all the components to the login page

--- supplier-frontend/src/pages/signup/Signup.js
+++ supplier-frontend/src/pages/signup/Signup.js
@@ -34,7 +34,7 @@ const Signup = () => {
             <TextInput label={"Password"} type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")}/>
             <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} value={user.current_password} onChange={(e)=>handleChange(e,"current_password")}/>
             <SignupLoginButton text={"SIGNUP"} onClick={handleClick}/>
-            <p className='font-sans'>Already Have an account?
+            <p className='font-sans'>Already have an account?
                 <span className='text-white cursor-pointer' onClick={() =>{navigate("/login")}}>
                     Login
                 </span>
