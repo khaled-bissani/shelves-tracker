3483485f887432da08ba4c3bf5b1168ce981baf4
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-24 13:32:24 +0300

    Frontend <> Supplier frontend: adding props to the text input component

--- supplier-frontend/src/components/TextInput.jsx
+++ supplier-frontend/src/components/TextInput.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const TextInput = ({label,type,placeholder}) => {
+const TextInput = ({label,type,placeholder,value,onChange}) => {
   return (
     <div className='flex flex-col'>
         <label className='font-sans text-[16px] text-white'>{label}</label>
-        <input className="outline-none w-[300px] h-[40px] pl-4 bg-white font-sans text-[14px]" type={type} placeholder={placeholder} />
+        <input className="outline-none w-[300px] h-[40px] pl-4 bg-white font-sans text-[14px]" type={type} placeholder={placeholder} value={value} onChange={onChange}/>
     </div>
   )
 }
