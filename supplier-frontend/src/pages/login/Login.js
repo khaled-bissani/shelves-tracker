191e4a956ff428bda0ed9d77dfa8ce924654949c
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 22:37:08 +0300

    Frontend <> Supplier frontend: editing login page

--- supplier-frontend/src/pages/login/Login.js
+++ supplier-frontend/src/pages/login/Login.js
@@ -28,9 +28,9 @@ const Login = () => {
     <div className='flex justify-end h-screen' style={{backgroundImage : `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
         <div className='flex flex-col items-center justify-around w-[375px] h-[450px] bg-[#3AA346] mt-28 mr-56'>
             <h1 className='font-sans font-bold text-[24px] text-white'>Login</h1>
-            <TextInput label={"Email"} type={"email"} placeholder={"Email"} value={user.email} onChange={(e)=>handleChange(e,"email")}/>
-            <TextInput label={"Password"} type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")}/>
-            <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} value={user.current_password} onChange={(e)=>handleChange(e,"current_password")}/>
+            <TextInput label={"Email"} type={"email"} placeholder={"Email"} value={user.email} onChange={(e)=>handleChange(e,"email")} textColor="white" bgColor="white"/>
+            <TextInput label={"Password"} type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")} textColor="white" bgColor="white"/>
+            <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} value={user.current_password} onChange={(e)=>handleChange(e,"current_password")} textColor="white" bgColor="white"/>
             <SignupLoginButton text={"LOGIN"} onClick={handleClick}/>
             <p className='font-sans'>Don't have an account?
                 <span className='text-white cursor-pointer' onClick={() =>{navigate("/")}}>
