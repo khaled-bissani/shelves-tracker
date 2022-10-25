3b22fed0786436d8c911abef49202bfc53884177
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 10:50:25 +0300

    Frontend <> Supplier frontend: creating chat contact component

--- supplier-frontend/src/pages/home/Chat.js
+++ supplier-frontend/src/pages/home/Chat.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import SideBar from '../../components/SideBar'
+import ChatContact from '../../components/ChatContact'
 
 const Chat = () => {
   return (
@@ -9,11 +10,7 @@ const Chat = () => {
             <p className='text-[24px] font-sans font-bold'>Chat</p>
             <div className='flex w-[900px] h-[400px] border border-[#868282]'>
                 <div className='flex flex-col w-1/4 overflow-auto'>
-                    <div className='flex justify-evenly my-4'>
-                        <div className='w-[60px] h-[60px] bg-[#D9D9D9]'></div>
-                        <p className='flex flex-col items-center justify-center'>name</p>
-                    </div>
-                    <div className='w-full border border-[#868282]'></div>
+                    <ChatContact name={"name"}/>
                 </div>
                 <div className='h-[400px] border border-[#868282]'></div>
                 <div></div>
