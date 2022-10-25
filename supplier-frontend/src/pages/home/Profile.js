91adc1175f875099846d3d505abeb20d5b2da865
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 17:36:03 +0300

    Frontend <> Supplier frontend: adding element to the profile page

--- supplier-frontend/src/pages/home/Profile.js
+++ supplier-frontend/src/pages/home/Profile.js
@@ -3,8 +3,18 @@ import SideBar from '../../components/SideBar'
 
 const Profile = () => {
   return (
-    <div>
+    <div className='flex'>
         <SideBar/>
+        <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
+            <p className='text-[24px] font-sans font-bold'>Profile</p>
+            <div className='flex justify-center w-[900px] h-[420px] bg-[#D9D9D9] rounded-[10px]'>
+                <div className='flex flex-col justify-between my-6 mr-6'>
+                    <div className='w-[400px] h-[175px] bg-white rounded-[10px]'></div>
+                    <div className='w-[400px] h-[175px] bg-white rounded-[10px]'></div>
+                </div>
+                <div className='w-[400px] h-[370px] bg-white rounded-[10px] my-6'></div>
+            </div>
+        </div>
     </div>
   )
 }
