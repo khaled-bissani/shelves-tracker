818dc913c17fed04a26af9083d8c801ddd19fa85
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 12:29:53 +0300

    Frontend <> Supplier frontend: editing the size of chat page

--- supplier-frontend/src/pages/home/Chat.js
+++ supplier-frontend/src/pages/home/Chat.js
@@ -11,11 +11,11 @@ const Chat = () => {
         <SideBar/>
         <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
             <p className='text-[24px] font-sans font-bold'>Chat</p>
-            <div className='flex w-[900px] h-[400px] border border-[#868282]'>
+            <div className='flex w-[900px] h-[420px] border border-[#868282]'>
                 <div className='flex flex-col w-1/3 overflow-auto'>
                     <ChatContact name={"name"}/>
                 </div>
-                <div className='h-[400px] border border-[#868282]'></div>
+                <div className='h-[420px] border border-[#868282]'></div>
                 <div className='flex flex-col justify-between w-2/3'>
                     <ChatHeader name={"name"}/>
                     <div className='flex flex-col items-center overflow-auto h-4/6 pt-6'>
