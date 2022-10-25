cb59ef17c768f11a23732ca7c6e6c53a27f93907
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 10:26:35 +0300

    Frontend <> Supplier frontend: adding chat contact in the chat page

--- supplier-frontend/src/pages/home/Chat.js
+++ supplier-frontend/src/pages/home/Chat.js
@@ -5,6 +5,20 @@ const Chat = () => {
   return (
     <div className='flex'>
         <SideBar/>
+        <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
+            <p className='text-[24px] font-sans font-bold'>Chat</p>
+            <div className='flex w-[900px] h-[400px] border border-[#868282]'>
+                <div className='flex flex-col w-1/4 overflow-auto'>
+                    <div className='flex justify-evenly my-4'>
+                        <div className='w-[60px] h-[60px] bg-[#D9D9D9]'></div>
+                        <p className='flex flex-col items-center justify-center'>name</p>
+                    </div>
+                    <div className='w-full border border-[#868282]'></div>
+                </div>
+                <div className='h-[400px] border border-[#868282]'></div>
+                <div></div>
+            </div>
+        </div>
     </div>
   )
 }
