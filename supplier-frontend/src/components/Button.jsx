0b98972304065540459c83910e294eccd3449957
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 22:53:32 +0300

    Frontend <> Supplier frontend: editing button component

--- supplier-frontend/src/components/Button.jsx
+++ supplier-frontend/src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import classnames  from "classnames";
 
-const Button = ({bgColor,opacity,name}) => {
+const Button = ({bgColor,opacity,width,height,name}) => {
   return (
     <div>
-        <button className={classnames(`bg-[${bgColor}] opacity-${opacity} w-[170px] h-[46px] text-white text-[16px] cursor-pointer`)}>{name}</button>
+        <button className={classnames(`bg-[${bgColor}] opacity-${opacity} w-[${width}] h-[${height}] text-white text-[16px] cursor-pointer`)}>{name}</button>
     </div>
   )
 }
