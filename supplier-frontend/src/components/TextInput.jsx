281f00d9816d5230a89f1d9afa7bc6cddc334ff2
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 22:25:09 +0300

    Frontend <> Supplier frontend: editing text input component

--- supplier-frontend/src/components/TextInput.jsx
+++ supplier-frontend/src/components/TextInput.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
-const TextInput = ({label,type,placeholder,value,onChange}) => {
+const TextInput = ({label,type,placeholder,value,onChange,textColor,bgColor}) => {
+
+  const labelClass=`font-sans text-[16px] text-${textColor}`
+  const inputClass=`outline-none w-[300px] h-[40px] pl-4 bg-${bgColor} font-sans text-[14px]`
+
   return (
     <div className='flex flex-col'>
-        <label className='font-sans text-[16px] text-white'>{label}</label>
-        <input className="outline-none w-[300px] h-[40px] pl-4 bg-white font-sans text-[14px]" type={type} placeholder={placeholder} value={value} onChange={onChange}/>
+        <label className={labelClass}>{label}</label>
+        <input className={inputClass} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
     </div>
   )
 }
