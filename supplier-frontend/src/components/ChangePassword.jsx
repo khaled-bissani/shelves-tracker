1e10bbefc5768044cb1ad7b67dece5e5eb0404f7
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 23:15:54 +0300

    Frontend <> Supplier frontend: adding text input and button in the change password component

--- supplier-frontend/src/components/ChangePassword.jsx
+++ supplier-frontend/src/components/ChangePassword.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const ChangePassword = () => {
   return (
-    <div>ChangePassword</div>
+    <div className='flex flex-col items-center justify-around h-full'>
+        <TextInput label={"Old Password"} type={"password"} placeholder={"Old Password"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"New Password"} type={"password"} placeholder={"New Password"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} textColor="black" bgColor="#D9D9D9"/>
+        <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"}/>
+    </div>
   )
 }
 
