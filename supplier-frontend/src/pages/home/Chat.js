0bde1e4f1cff18260e5ef083576832d11e6b8877
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 12:14:00 +0300

    Frontend <> Supplier frontend: adding message in the chat page

--- supplier-frontend/src/pages/home/Chat.js
+++ supplier-frontend/src/pages/home/Chat.js
@@ -17,7 +17,13 @@ const Chat = () => {
                 <div className='h-[400px] border border-[#868282]'></div>
                 <div className='flex flex-col justify-between w-2/3'>
                     <ChatHeader name={"name"}/>
-                    <div className='flex flex-col items-center overflow-auto h-4/6 pt-6'></div>
+                    <div className='flex flex-col items-center overflow-auto h-4/6 pt-6'>
+                        <div className='flex flex-col justify-between w-3/4  bg-[#D9D9D9] mb-6 px-4'>
+                            <p className='text-[16px] font-sans h-1/4'>name</p>
+                            <p className='text-[20px] font-sans h-full'>message</p>
+                            <p className='flex justify-end text-[12px] font-sans h-1/4'>time</p>
+                        </div>
+                    </div>
                     <ChatSend/>
                 </div>
             </div>
