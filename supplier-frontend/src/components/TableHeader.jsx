602a1580ae296392eb109f31f2860a5126e566ea
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 16:37:39 +0300

    Frontend <> Supplier frontend: adding table header component in the client page

--- supplier-frontend/src/components/TableHeader.jsx
+++ supplier-frontend/src/components/TableHeader.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
-const TableHeader = () => {
+const TableHeader = ({column1, column2 ,column3}) => {
+    const paragraphClass= `flex flex-col items-center justify-center text-white text-[16px] px-[15px]`
+
   return (
-    <div>TableHeader</div>
+    <div>
+        <div className='flex justify-between h-[70px] bg-[#3AA346]'>
+            <p className={paragraphClass}>{column1}</p>
+            <p className={paragraphClass}>{column2}</p>
+            <p className={paragraphClass}>{column3}</p>
+        </div>
+    </div>
   )
 }
 
--- supplier-frontend/src/pages/home/Client.js
+++ supplier-frontend/src/pages/home/Client.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from '../../components/Button'
 import InputField from '../../components/InputField'
 import SideBar from '../../components/SideBar'
+import TableHeader from '../../components/TableHeader'
 
 const Client = () => {
   return (
@@ -9,7 +10,7 @@ const Client = () => {
         <SideBar/>
         <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
             <p className='text-[24px] font-sans font-bold'>Client</p>
-            <div className='flex w-[900px] h-[420px] border border-[#868282]'>
+            <div className='flex flex-col justify-between w-[900px] h-[420px] border border-[#868282]'>
                 <div className='flex justify-between w-full'>
                     <InputField placeholder={"Client Name"} />
                     <Button bgColor="#3AA346" name={"ADD"}/>
@@ -17,6 +18,9 @@ const Client = () => {
                     <Button bgColor="#FF0000" opacity="70" name={"DELETE"}/>
                     <Button bgColor="#3AA346" name={"CHAT"}/>
                 </div> 
+                <div>
+                    <TableHeader column1={"Client photo"} column2={"Client name"} column3={"Client email"} />
+                </div>
             </div>
         </div>
     </div>
