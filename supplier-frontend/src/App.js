5d4a23ce860198da09112a3f6904c360659d7923
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 16:57:35 +0300

    Frontend <> Supplier frontend: linking expiry page with the sidebar

--- supplier-frontend/src/App.js
+++ supplier-frontend/src/App.js
@@ -3,6 +3,7 @@ import Login from "./pages/login/Login";
 import Home from "./pages/home/Home";
 import Chat from "./pages/home/Chat";
 import Client from "./pages/home/Client";
+import Expiry from "./pages/home/Expiry";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/home" element={<Home/>} />
           <Route path="/chat" element={<Chat/>} />
           <Route path="/client" element={<Client/>} />
+          <Route path="/expiry" element={<Expiry/>} />
         </Routes>
       </Router>
     </div>
