5a2842b629458ffc4ea5946af943c14f8591b040
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-25 12:19:19 +0300

    Frontend <> Supplier frontend: creating chat message component

--- supplier-frontend/src/pages/home/Chat.js
+++ supplier-frontend/src/pages/home/Chat.js
@@ -3,6 +3,7 @@ import SideBar from '../../components/SideBar'
 import ChatContact from '../../components/ChatContact'
 import ChatHeader from '../../components/ChatHeader'
 import ChatSend from '../../components/ChatSend'
+import ChatMessage from '../../components/ChatMessage'
 
 const Chat = () => {
   return (
@@ -18,11 +19,7 @@ const Chat = () => {
                 <div className='flex flex-col justify-between w-2/3'>
                     <ChatHeader name={"name"}/>
                     <div className='flex flex-col items-center overflow-auto h-4/6 pt-6'>
-                        <div className='flex flex-col justify-between w-3/4  bg-[#D9D9D9] mb-6 px-4'>
-                            <p className='text-[16px] font-sans h-1/4'>name</p>
-                            <p className='text-[20px] font-sans h-full'>message</p>
-                            <p className='flex justify-end text-[12px] font-sans h-1/4'>time</p>
-                        </div>
+                        <ChatMessage name={"name"} message={"message"} time={"time"}/>
                     </div>
                     <ChatSend/>
                 </div>
