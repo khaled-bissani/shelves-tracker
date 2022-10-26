85a792e216d56436fc39db0e5f7df3b402badda0
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-26 09:15:16 +0300

    Frontend <> Supplier frontend: adding props to the input field component

--- supplier-frontend/src/components/InputField.jsx
+++ supplier-frontend/src/components/InputField.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const InputField = ({placeholder}) => {
+const InputField = ({placeholder,value,onChange}) => {
   return (
     <div>
-        <input className='w-[170px] h-[46px] text-[18px] pl-[10px] border border-black outline-none' type={"text"} placeholder={placeholder} />
+        <input className='w-[170px] h-[46px] text-[18px] pl-[10px] border border-black outline-none' type={"text"} placeholder={placeholder} value={value} onChange={onChange}/>
     </div>
   )
 }
