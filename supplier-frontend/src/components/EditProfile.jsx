6878eaccdd304010ccb449bae69d95fc464974a5
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-26 09:52:18 +0300

    Frontend <> Supplier frontend: handling the button click in the edit profile component

--- supplier-frontend/src/components/EditProfile.jsx
+++ supplier-frontend/src/components/EditProfile.jsx
@@ -18,13 +18,17 @@ const EditProfile = () => {
     });
   }
 
+  const handleClick = () => {
+    console.log('save')
+  }
+
   return (
     <div className='flex flex-col items-center justify-around h-full'>
         <TextInput label={"Name"} type={"text"} placeholder={"Name"} textColor="black" bgColor="#D9D9D9" value={profile.name} onChange={(e)=>handleChange(e,"name")}/>
         <TextInput label={"Email"} type={"text"} placeholder={"Email"} textColor="black" bgColor="#D9D9D9" value={profile.email} onChange={(e)=>handleChange(e,"email")}/>
         <TextInput label={"Phone Number"} type={"text"} placeholder={"Phone Number"} textColor="black" bgColor="#D9D9D9" value={profile.phone_number} onChange={(e)=>handleChange(e,"phone_number")}/>
         <TextInput label={"Address"} type={"text"} placeholder={"Address"} textColor="black" bgColor="#D9D9D9" value={profile.address} onChange={(e)=>handleChange(e,"address")}/>
-        <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"}/>
+        <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"} onClick={handleClick}/>
     </div>
   )
 }
