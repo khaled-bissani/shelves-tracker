d069ecccaa3de6cd8c43994ac62559f98fe42251
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-26 10:38:40 +0300

    Frontend <> Supplier frontend: editing profile components

--- supplier-frontend/src/components/ProfileInformation.jsx
+++ supplier-frontend/src/components/ProfileInformation.jsx
@@ -3,7 +3,7 @@ import TextInput from './TextInput'
 
 const ProfileInformation = () => {
   return (
-    <div className='flex flex-col items-center justify-around h-full'>
+    <div className='flex flex-col items-center justify-around w-[400px] h-[370px] bg-white rounded-[10px] my-6'>
         <TextInput label={"Name"} type={"text"} placeholder={"Name"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
         <TextInput label={"Email"} type={"text"} placeholder={"Email"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
         <TextInput label={"Phone Number"} type={"text"} placeholder={"Phone Number"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
