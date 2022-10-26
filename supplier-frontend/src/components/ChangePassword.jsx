f1546c5d86e454c078d0fa155526e8b1589ca7a9
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-26 09:58:04 +0300

    Frontend <> Supplier frontend: handling the value of the input text in the change password component

--- supplier-frontend/src/components/ChangePassword.jsx
+++ supplier-frontend/src/components/ChangePassword.jsx
@@ -16,9 +16,9 @@ const ChangePassword = () => {
 
   return (
     <div className='flex flex-col items-center justify-around h-full'>
-        <TextInput label={"Old Password"} type={"password"} placeholder={"Old Password"} textColor="black" bgColor="#D9D9D9"/>
-        <TextInput label={"New Password"} type={"password"} placeholder={"New Password"} textColor="black" bgColor="#D9D9D9"/>
-        <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"Old Password"} type={"password"} placeholder={"Old Password"} textColor="black" bgColor="#D9D9D9" value={password.old_password} onChange={(e)=>handleChange(e,"old_password")}/>
+        <TextInput label={"New Password"} type={"password"} placeholder={"New Password"} textColor="black" bgColor="#D9D9D9" value={password.new_password} onChange={(e)=>handleChange(e,"new_password")}/>
+        <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} textColor="black" bgColor="#D9D9D9" value={password.confirm_password} onChange={(e)=>handleChange(e,"confirm_password")}/>
         <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"}/>
     </div>
   )
