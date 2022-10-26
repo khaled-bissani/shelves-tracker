674cf773a40842674b456707c59beb69cafebec2
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-26 10:19:34 +0300

    Frontend <> Supplier frontend: adding read only attribute to the text input inside profile information component

--- supplier-frontend/src/components/ProfileInformation.jsx
+++ supplier-frontend/src/components/ProfileInformation.jsx
@@ -4,10 +4,10 @@ import TextInput from './TextInput'
 const ProfileInformation = () => {
   return (
     <div className='flex flex-col items-center justify-around h-full'>
-        <TextInput label={"Name"} type={"text"} placeholder={"Name"} textColor="black" bgColor="#D9D9D9"/>
-        <TextInput label={"Email"} type={"text"} placeholder={"Email"} textColor="black" bgColor="#D9D9D9"/>
-        <TextInput label={"Phone Number"} type={"text"} placeholder={"Phone Number"} textColor="black" bgColor="#D9D9D9"/>
-        <TextInput label={"Address"} type={"text"} placeholder={"Address"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"Name"} type={"text"} placeholder={"Name"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"Email"} type={"text"} placeholder={"Email"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"Phone Number"} type={"text"} placeholder={"Phone Number"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
+        <TextInput label={"Address"} type={"text"} placeholder={"Address"} readOnly={"readOnly"} textColor="black" bgColor="#D9D9D9"/>
     </div>
   )
 }
--- supplier-frontend/src/components/TextInput.jsx
+++ supplier-frontend/src/components/TextInput.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TextInput = ({label,type,placeholder,value,onChange,textColor,bgColor}) => {
+const TextInput = ({label,type,placeholder,value,onChange,textColor,bgColor,readOnly}) => {
 
   const labelClass=`font-sans text-[16px] text-${textColor}`
   const inputClass=`outline-none w-[300px] h-[40px] pl-4 bg-[${bgColor}] font-sans text-[14px]`
@@ -8,7 +8,7 @@ const TextInput = ({label,type,placeholder,value,onChange,textColor,bgColor}) =>
   return (
     <div className='flex flex-col'>
         <label className={labelClass}>{label}</label>
-        <input className={inputClass} type={type} placeholder={placeholder} value={value} onChange={onChange}/>
+        <input className={inputClass} type={type} placeholder={placeholder} value={value} onChange={onChange} readOnly={readOnly}/>
     </div>
   )
 }
