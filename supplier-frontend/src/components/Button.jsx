1f371807d12086ee1e334763d5ee82e344464339
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-26 09:21:54 +0300

    Frontend <> Supplier frontend: handling the value of the input text in the client page

--- supplier-frontend/src/components/Button.jsx
+++ supplier-frontend/src/components/Button.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Button = ({bgColor,opacity,width,height,name}) => {
+const Button = ({bgColor,opacity,width,height,onClick,name}) => {
 
   const buttonClass=`bg-[${bgColor}] opacity-${opacity} w-[${width}] h-[${height}] text-white text-[16px] cursor-pointer`
 
   return (
     <div>
-        <button className={buttonClass}>{name}</button>
+        <button className={buttonClass} onClick={onClick}>{name}</button>
     </div>
   )
 }
