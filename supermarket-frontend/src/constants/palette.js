f78899412a61cd0cf432422e35cd7ef626aa5935
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-28 15:22:22 +0300

    Frontend <> Supermarket frontend: adding colors in the palette file

--- supermarket-frontend/src/constants/palette.js
+++ supermarket-frontend/src/constants/palette.js
@@ -1,5 +1,10 @@
 export const colors = {
-
+    primary: '#3AA346',
+    secondary: '#6EDA79',
+    black: '#000000',
+    white: '#FFFFFF',
+    light_gray: '#D9D9D9',
+    background: '#EEEEEE'
 }
 
 export default colors;
