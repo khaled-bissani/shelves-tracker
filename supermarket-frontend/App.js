2bbbdf023043dc9cdaff35203505db09f55ca602
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-28 16:11:36 +0300

    Frontend <> Supermarket frontend: adding style in the main file

--- supermarket-frontend/App.js
+++ supermarket-frontend/App.js
@@ -1,10 +1,16 @@
-import {View } from 'react-native';
+import {StyleSheet, View } from 'react-native';
 import Landing from './src/pages/landing/Landing';
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.mainContainer}>
       <Landing/>
     </View>
   );
 }
+
+const styles= StyleSheet.create({
+  mainContainer: {
+    flex:1
+  }
+})
