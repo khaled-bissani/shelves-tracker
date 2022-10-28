53d0c9a0eda64256ce91e4dc3d3438fd3b2125b5
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-28 15:53:04 +0300

    Frontend <> Supermarket frontend: adding title in the landing page

--- supermarket-frontend/src/pages/landing/Landing.js
+++ supermarket-frontend/src/pages/landing/Landing.js
@@ -1,10 +1,11 @@
-import { Image, View } from "react-native";
+import { Image, Text, View } from "react-native";
 import styles from "./styles";
 
 const Landing = () => {
     return(
         <View style={styles.landingContainer}>
             <Image source={require('../../../assets/images/landing.png')} style={styles.image}/>
+            <Text style={styles.titleText}>Shelves Tracker</Text>
         </View>
     )
 }
--- supermarket-frontend/src/pages/landing/styles.js
+++ supermarket-frontend/src/pages/landing/styles.js
@@ -1,13 +1,19 @@
 import { StyleSheet } from 'react-native';
+import colors from '../../constants/palette';
 
 export default StyleSheet.create({
     landingContainer:{
         flexDirection:'column',
         alignContent:'center',
-        justifyContent:'center'
+        justifyContent:'center',
+        width:'100%',
     },
     image:{
         width:'70%',
         height:'75%'
+    },
+    titleText:{
+        color: colors.primary,
+        
     }
 });
