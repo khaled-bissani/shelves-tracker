b06cbb907f8da96a9524467fb35c0496f455b3d4
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-29 22:41:42 +0300

    Frontend <> Supermarket frontend: adding element inside the pressable text component

--- supermarket-frontend/src/components/PressableText/PressableText.js
+++ supermarket-frontend/src/components/PressableText/PressableText.js
@@ -1,5 +1,11 @@
-const PressableText = () => {
+import { Pressable, Text } from "react-native";
 
+const PressableText = (props) => {
+    return(
+        <Pressable onPress={props.onClick}>
+            <Text style={{color:props.textColor, fontSize:props.fontSize}}>{props.text}</Text>
+        </Pressable>
+    )
 }
 
 export default PressableText;
