5d497a66098d59ac673f896ba976405d5f119626
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-29 23:11:06 +0200

    Frontend <> Supermarket frontend: adding elements in the google button component

--- supermarket-frontend/src/components/GoogleButton/GoogleButton.js
+++ supermarket-frontend/src/components/GoogleButton/GoogleButton.js
@@ -1,5 +1,14 @@
-const GoogleButton = () => {
+import { Image, Pressable, Text, View } from "react-native";
 
+const GoogleButton = (props) => {
+    return(
+        <View>
+            <Pressable onPress={props.onClick}>
+                <Image source={require('../../../assets/images/google.png')}/> 
+                <Text>{props.text}</Text>
+            </Pressable>
+        </View>
+    )
 }
 
 export default GoogleButton;
