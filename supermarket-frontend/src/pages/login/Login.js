817e01fc126673213849b45c87f97077aedd02cd
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-29 23:46:56 +0200

    Frontend <> Supermarket frontend: adding element to the login page

--- supermarket-frontend/src/pages/login/Login.js
+++ supermarket-frontend/src/pages/login/Login.js
@@ -1,10 +1,21 @@
 import { Text, View } from "react-native";
+import Buttons from "../../components/Button/Buttons";
+import GoogleButton from "../../components/GoogleButton/GoogleButton";
+import InputField from "../../components/InputField/InputField";
+import { colors, fonts } from "../../constants/palette";
 import styles from "./styles";
 
 const Login = () => {
     return(
         <View>
-            <Text>Login</Text>
+            <View>
+                <InputField type={"default"} placeholder={"Username"}/>
+                <InputField type={"default"} secureTextEntry={true} placeholder={"Password"}/>
+                <InputField type={"default"} secureTextEntry={true} placeholder={"Current Password"}/>
+            </View>
+            <Buttons text={'LOGIN'} color={colors.primary}/>
+            <Text>or</Text>
+            <GoogleButton text={'Continue with Google'}/>
         </View>
     )
 }
