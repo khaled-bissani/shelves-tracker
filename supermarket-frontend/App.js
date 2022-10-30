cbe73cd528b8789c410e200cf961db3025cc2142
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-30 18:19:34 +0200

    Frontend <> Supermarket frontend: creating main stack navigation

--- supermarket-frontend/App.js
+++ supermarket-frontend/App.js
@@ -1,19 +1,15 @@
 import {Pressable, StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
-import Landing from './src/pages/landing/Landing';
-import Login from './src/pages/login/Login';
-import Signup from './src/pages/signup/Signup';
 import Home from './src/pages/home/Home';
 import { colors } from './src/constants/palette';
 import { Ionicons } from '@expo/vector-icons';
 import Notification from './src/pages/notification/Notification';
 import Chat from './src/pages/chat/Chat';
 import Profile from './src/pages/profile/Profile';
+import MainStackNavigation from './src/navigation/MainStackNavigation';
 
-const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
@@ -60,19 +56,7 @@ export default function App() {
   return (
     <>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{
-          headerStyle:{backgroundColor: '#EEEEEE', },
-          headerTitleAlign:'center',
-          headerTitleStyle:{fontWeight: 'bold'},
-          contentStyle:{backgroundColor: '#EEEEEE'},          
-        }}>
-          <Stack.Screen name='Landing' component={Landing} options={{headerShown: false}}/>
-          <Stack.Screen name='Register' component={Signup}/>
-          <Stack.Screen name='Login' component={Login}/>
-          <Stack.Screen name='Main' component={DrawerNavigator} options={{
-            headerShown:false
-          }}/>
-        </Stack.Navigator>
+        <MainStackNavigation/>
       </NavigationContainer>
     </>    
   );
---
+++ supermarket-frontend/src/navigation/MainStackNavigation.js
@@ -0,0 +1,25 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Landing from '../pages/landing/Landing';
+import Login from '../pages/login/Login';
+import Signup from '../pages/signup/Signup';
+import DrawerNavigator from '../../App'
+
+const Stack = createNativeStackNavigator();
+
+const MainStackNavigation = () => {
+    return <Stack.Navigator screenOptions={{
+        headerStyle:{backgroundColor: '#EEEEEE', },
+        headerTitleAlign:'center',
+        headerTitleStyle:{fontWeight: 'bold'},
+        contentStyle:{backgroundColor: '#EEEEEE'},          
+      }}>
+        <Stack.Screen name='Landing' component={Landing} options={{headerShown: false}}/>
+        <Stack.Screen name='Register' component={Signup}/>
+        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name='Main' component={DrawerNavigator} options={{
+          headerShown:false
+        }}/>
+      </Stack.Navigator>
+}
+
+export default MainStackNavigation;
