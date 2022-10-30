222f30e0714ac012c50a46006a20982322b6747c
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-30 15:48:46 +0200

    Frontend <> Supermarket frontend: adding navigation for the notification page

--- supermarket-frontend/App.js
+++ supermarket-frontend/App.js
@@ -8,6 +8,7 @@ import Signup from './src/pages/signup/Signup';
 import Home from './src/pages/home/Home';
 import { colors } from './src/constants/palette';
 import { Ionicons } from '@expo/vector-icons';
+import Notification from './src/pages/notification/Notification';
 
 const Stack = createNativeStackNavigator();
 const BottomTab = createBottomTabNavigator();
@@ -26,6 +27,9 @@ const BottomTabNavigator = () => {
   }}>
     <BottomTab.Screen name='Home' component={Home} options={{
       tabBarIcon: ({color, size}) => <Ionicons name='home' color={color} size={size}/>
+    }}/>
+    <BottomTab.Screen name='Notification' component={Notification} options={{
+      tabBarIcon: ({color, size}) => <Ionicons name='notifications-sharp' color={color} size={size}/>
     }}/>
   </BottomTab.Navigator>
 }
