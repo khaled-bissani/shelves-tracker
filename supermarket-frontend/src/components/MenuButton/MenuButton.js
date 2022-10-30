2dfc431b40c8502b306c50ad34bc6ee9f8b61555
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-30 18:48:13 +0200

    Frontend <> Supermarket frontend: styling the menu button component

--- supermarket-frontend/src/components/MenuButton/MenuButton.js
+++ supermarket-frontend/src/components/MenuButton/MenuButton.js
@@ -1,10 +1,11 @@
 import { Pressable } from "react-native";
 import { Ionicons } from '@expo/vector-icons';
 import { colors } from '../../constants/palette';
+import styles from "./styles";
 
 const MenuButton = (props) => {
-    return <Pressable onPress={props.onPress}>
-            <Ionicons name='menu' size={36} color={colors.white} />
+    return <Pressable onPress={props.onPress} style={styles.buttonContainer}>
+            <Ionicons name='menu' size={36} style={styles.icon} />
         </Pressable>
 }
 
