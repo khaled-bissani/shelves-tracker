1a0ad310131cc3c2def1ef5daf0b92d7069a3ce9
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-31 10:35:44 +0200

    Frontend <> Supermarket frontend: adding element in the home page

--- supermarket-frontend/src/pages/home/Home.js
+++ supermarket-frontend/src/pages/home/Home.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import Categories from "../../components/Categories/Categories";
 
 const Home = () => {
-    return <Text>Home Page</Text>
+    return <>
+    <View>
+        <View>
+            <Text>All Categories</Text>
+        </View>
+        <Categories/>
+
+    </View>
+    </>
 }
 
 export default Home;
