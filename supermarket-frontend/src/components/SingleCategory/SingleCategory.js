25166ef2b3d43fcfbb722eb89e121a3cbe3970e8
khaled-bissani <khaled.w.bissani@gmail.com>
2022-10-31 20:29:47 +0200

    Frontend <> Supermarket frontend: styling the single category component

--- supermarket-frontend/src/components/SingleCategory/SingleCategory.js
+++ supermarket-frontend/src/components/SingleCategory/SingleCategory.js
@@ -1,15 +1,16 @@
 import { Text, View, ScrollView } from "react-native";
 import PressableText from "../PressableText/PressableText";
 import SingleItem from "../SingleItem/SingleItem";
+import styles from "./styles";
 
 const SingleCategory = (props) => {
-    return <View>
-        <View>
-            <Text>{props.categoryTitle}</Text>
+    return <View style={styles.singleCategoryContainer}>
+        <View style={styles.singleCategoryTitleContainer}>
+            <Text style={styles.singleCtegoryTitle}>{props.categoryTitle}</Text>
             <PressableText text={'view more'} textColor={'#545454'} fontSize={12}/>
         </View>
         <ScrollView horizontal={true}>
-            <View >
+            <View style={styles.singleItemContainer}>
                 <SingleItem label={"item name"}/>
             </View>
         </ScrollView>
--- supermarket-frontend/src/components/SingleCategory/styles.js
+++ supermarket-frontend/src/components/SingleCategory/styles.js
@@ -2,5 +2,19 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../constants/palette";
 
 export default StyleSheet.create({
-    
+    singleCategoryContainer:{
+        marginTop:25
+    },
+    singleCategoryTitleContainer:{
+        flexDirection:'row',
+        justifyContent:'space-between',
+        marginBottom:25
+    },
+    singleCtegoryTitle:{
+        fontSize: fonts.subTitle,
+        fontWeight: fonts.bold
+    },
+    singleItemContainer:{
+        flexDirection:'row',
+    }
 })
