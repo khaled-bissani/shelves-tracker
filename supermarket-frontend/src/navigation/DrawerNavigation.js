d9dd8c0964babab6fad7ae5ee55e6e5815b87356
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-01 13:16:21 +0200

    Frontend <> Supermarket frontend: styling the drawer navigation

--- supermarket-frontend/src/navigation/DrawerNavigation.js
+++ supermarket-frontend/src/navigation/DrawerNavigation.js
@@ -1,13 +1,23 @@
+import { Ionicons } from '@expo/vector-icons';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { colors, fonts } from '../constants/palette';
 import BottomTabNavigation from './BottomTabNavigation';
 
 const Drawer = createDrawerNavigator();
 
 const DrawerNavigator = () => {
     return <Drawer.Navigator screenOptions={{
-      headerShown:false
+      headerShown:false,
+      drawerActiveBackgroundColor: colors.primary,
+      drawerActiveTintColor: colors.white,
+      drawerInactiveTintColor: colors.secondary,
+      drawerStyle: {backgroundColor: colors.background},
+      drawerLabelStyle: {fontSize: fonts.text}
     }}>
-      <Drawer.Screen name='DrawerHome' component={BottomTabNavigation}/>
+      <Drawer.Screen name='DrawerHome' component={BottomTabNavigation} options={{
+        drawerIcon: ({color}) => <Ionicons name='home' color={color} size={fonts.iconSize}/>,
+        drawerLabel: 'Home'
+      }}/>
     </Drawer.Navigator>
 }
 
