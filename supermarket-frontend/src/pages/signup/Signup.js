6f95de0e6091e2228141b60461117f2c3de24ea5
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-01 11:36:54 +0200

    Frontend <> Supermarket frontend: adding scroll view to the signup page

--- supermarket-frontend/src/pages/signup/Signup.js
+++ supermarket-frontend/src/pages/signup/Signup.js
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, View, ScrollView } from "react-native";
 import Buttons from "../../components/Button/Buttons";
 import GoogleButton from "../../components/GoogleButton/GoogleButton";
 import InputField from "../../components/InputField/InputField";
@@ -17,25 +17,27 @@ const Signup = ({navigation}) => {
     }
 
     return(
-        <View style={styles.signup}>
-            <View style={styles.inputs}>
-                <InputField type={"default"} placeholder={"Full Name"}/>
-                <InputField type={"default"} placeholder={"Username"}/>
-                <InputField type={"email-address"} placeholder={"Email"}/>
-                <InputField type={"number-pad"} placeholder={"Phone Number"}/>
-                <InputField type={"default"} secureTextEntry={true} placeholder={"Password"}/>
-                <InputField type={"default"} secureTextEntry={true} placeholder={"Current Password"}/>
+        <ScrollView contentContainerStyle={{flexGrow: 1}}>
+            <View style={styles.signup}>
+                <View style={styles.inputs}>
+                    <InputField type={"default"} placeholder={"Full Name"}/>
+                    <InputField type={"default"} placeholder={"Username"}/>
+                    <InputField type={"email-address"} placeholder={"Email"}/>
+                    <InputField type={"number-pad"} placeholder={"Phone Number"}/>
+                    <InputField type={"default"} secureTextEntry={true} placeholder={"Password"}/>
+                    <InputField type={"default"} secureTextEntry={true} placeholder={"Current Password"}/>
+                </View>
+                <Buttons text={'CREATE ACCOUNT'} color={colors.primary} onClick={signupHandler}/>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>
+                            Already have an account?
+                    </Text>
+                    <PressableText text={"Login"} textColor={colors.primary} fontSize={fonts.text} onClick={goToLogin}/>
+                </View>  
+                <View style={styles.horizontalLine}></View>
+                <GoogleButton text={'Sign Up with Google'}/>
             </View>
-            <Buttons text={'CREATE ACCOUNT'} color={colors.primary} onClick={signupHandler}/>
-            <View style={styles.textContainer}>
-                <Text style={styles.text}>
-                        Already have an account?
-                </Text>
-                <PressableText text={"Login"} textColor={colors.primary} fontSize={fonts.text} onClick={goToLogin}/>
-            </View>  
-            <View style={styles.horizontalLine}></View>
-            <GoogleButton text={'Sign Up with Google'}/>
-        </View>
+        </ScrollView>
     )
 }
 
