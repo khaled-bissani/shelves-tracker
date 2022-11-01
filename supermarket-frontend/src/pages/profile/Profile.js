9afe9278ad544663e18bf833865b9d563e19fa54
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-01 11:10:02 +0200

    Frontend <> Supermarket frontend: styling the profile page

--- supermarket-frontend/src/pages/profile/Profile.js
+++ supermarket-frontend/src/pages/profile/Profile.js
@@ -1,14 +1,17 @@
 import { Text, View, Image} from "react-native";
 import ProfileOption from "../../components/ProfileOption/ProfileOption";
+import styles from "./styles";
 
 const Profile = () => {
     return <View>
-        <View>
-            <Image source={''}/>
+        <View style={styles.container}>
+            <View style={styles.profilePictureContainer}>
+                <Image style={styles.profilePicture} source={''}/>
+            </View>
+            <Text style={styles.profileName}>Supermarket</Text>
+            <View style={styles.profileLine}></View>
         </View>
-        <Text>Spermarket</Text>
-        <View></View>
-        <View>
+        <View style={styles.profileOptionContainer}>
             <ProfileOption icon={"create-outline"} text={"Edit profile"}/>
             <ProfileOption icon={"person-circle-outline"} text={"Change profile picture"}/>
             <ProfileOption icon={"lock-closed-sharp"} text={"Change password"}/>
--- supermarket-frontend/src/pages/profile/styles.js
+++ supermarket-frontend/src/pages/profile/styles.js
@@ -2,5 +2,35 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../constants/palette";
 
 export default StyleSheet.create({
-    
+    container: {
+        marginVertical:30,
+        height:250,
+        flexDirection:'column',
+        justifyContent:'space-evenly',
+        alignItems:'center'
+    },
+    profilePictureContainer: {
+        backgroundColor: colors.light_gray,
+        width: 150,
+        height:150,
+        borderRadius:100
+    },
+    profilePicture: {
+        width:'100%',
+        height:'100%',
+        resizeMode:"contain"
+    },
+    profileName: {
+        fontSize: fonts.title
+    },
+    profileLine: {
+        width:'100%',
+        borderBottomWidth: 2,
+        borderBottomColor: colors.light_gray
+    },
+    profileOptionContainer: {
+        height:250,
+        flexDirection:'column',
+        justifyContent:'space-evenly'
+    }
 })
