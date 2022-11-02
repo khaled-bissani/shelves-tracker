89ff85c0ecb562df67babba3536330d94c83eb2f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-02 11:15:44 +0200

    Frontend <> Supermarket frontend: adding element in the edit profile page

--- supermarket-frontend/src/pages/editProfile/EditProfile.js
+++ supermarket-frontend/src/pages/editProfile/EditProfile.js
@@ -1,7 +1,19 @@
-import { Text } from "react-native";
+import { View, Image } from "react-native";
+import { colors } from "../../constants/palette";
+import Buttons from "../../components/Button/Buttons";
+import InputField from "../../components/InputField/InputField";
 
 const EditProfile = () => {
-    return <Text>Edit Profile</Text>
+    return <View>
+        <View>
+            <Image source={''}/>
+        </View>
+        <InputField type={"default"} placeholder={"Full Name"}/>
+        <InputField type={"default"} placeholder={"Username"}/>
+        <InputField type={"email-address"} placeholder={"Email"}/>
+        <InputField type={"number-pad"} placeholder={"Phone Number"}/>
+        <Buttons text={'SAVE'} color={colors.primary}/>
+    </View>
 }
 
 export default EditProfile;
