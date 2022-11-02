04711be061e7d5db5c075b107d116d265648ee3f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-02 14:01:42 +0200

    Frontend <> Supermarket frontend: styling the edit profile

--- supermarket-frontend/src/pages/editProfile/EditProfile.js
+++ supermarket-frontend/src/pages/editProfile/EditProfile.js
@@ -2,16 +2,19 @@ import { View, Image } from "react-native";
 import { colors } from "../../constants/palette";
 import Buttons from "../../components/Button/Buttons";
 import InputField from "../../components/InputField/InputField";
+import styles from "./styles";
 
 const EditProfile = () => {
-    return <View>
-        <View>
-            <Image source={''}/>
+    return <View style={styles.editProfileContainer}>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={''}/>
         </View>
-        <InputField type={"default"} placeholder={"Full Name"}/>
-        <InputField type={"default"} placeholder={"Username"}/>
-        <InputField type={"email-address"} placeholder={"Email"}/>
-        <InputField type={"number-pad"} placeholder={"Phone Number"}/>
+        <View style={styles.inputs}>
+            <InputField type={"default"} placeholder={"Full Name"}/>
+            <InputField type={"default"} placeholder={"Username"}/>
+            <InputField type={"email-address"} placeholder={"Email"}/>
+            <InputField type={"number-pad"} placeholder={"Phone Number"}/>
+        </View>
         <Buttons text={'SAVE'} color={colors.primary}/>
     </View>
 }
--- supermarket-frontend/src/pages/editProfile/styles.js
+++ supermarket-frontend/src/pages/editProfile/styles.js
@@ -2,5 +2,26 @@ import { StyleSheet } from "react-native";
 import { colors } from "../../constants/palette";
 
 export default StyleSheet.create({
-    
+    editProfileContainer: {
+        justifyContent:'space-around',
+        alignItems:'center',
+        height:650
+    },
+    imageContainer: {
+        backgroundColor: colors.light_gray,
+        width: 150,
+        height:150,
+        borderRadius:100
+    },
+    image:{
+        width:'100%',
+        height:'100%',
+        resizeMode:"contain"
+    },
+    inputs:{
+        width:'100%',
+        justifyContent:'space-around',
+        alignItems:'center',
+        // marginTop:20
+    },
 })
