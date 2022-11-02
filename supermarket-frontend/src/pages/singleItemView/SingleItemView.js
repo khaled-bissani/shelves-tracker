e51f025300cb55259979b388b629b6f92211801a
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-02 14:45:08 +0200

    Frontend <> Supermarket frontend: adding elements in the single item view page

--- supermarket-frontend/src/pages/singleItemView/SingleItemView.js
+++ supermarket-frontend/src/pages/singleItemView/SingleItemView.js
@@ -1,7 +1,20 @@
-import { Text } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { Text, View, Image } from "react-native";
 
 const SingleItemView = () => {
-    return <Text>Single Item View</Text>
+    return <View>
+        <View>
+            <View>
+                <Image source={''}/>
+            </View>
+        </View>
+        <View>
+            <View>
+                <Text>Item Name</Text>
+                <Ionicons name="create-outline"/>
+            </View>
+        </View>
+    </View>
 }
 
 export default SingleItemView;
