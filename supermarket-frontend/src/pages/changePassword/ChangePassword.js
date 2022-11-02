4a87d3b2408edf6e73d0f14cfcf21348fb418348
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-02 14:06:11 +0200

    Frontend <> Supermarket frontend: adding element to the change password page

--- supermarket-frontend/src/pages/changePassword/ChangePassword.js
+++ supermarket-frontend/src/pages/changePassword/ChangePassword.js
@@ -1,7 +1,17 @@
-import { Text } from "react-native";
+import { View } from "react-native";
+import { colors } from "../../constants/palette";
+import InputField from "../../components/InputField/InputField";
+import Buttons from "../../components/Button/Buttons";
 
 const ChangePassword = () => {
-    return <Text>Change Password</Text>
+    return <View>
+    <View>
+        <InputField type={"default"} secureTextEntry={true} placeholder={"Old Password"}/>
+        <InputField type={"default"} secureTextEntry={true} placeholder={"New Password"}/>
+        <InputField type={"default"} secureTextEntry={true} placeholder={"Confirm Password"}/>
+    </View>
+    <Buttons text={'SAVE'} color={colors.primary}/>
+</View>
 }
 
 export default ChangePassword;
