cf3b49c53b2c95326edb9c206f81aefa94583575
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-02 16:27:47 +0200

    Frontend <> Supermarket frontend: adding sort icon in the view more item

--- supermarket-frontend/src/pages/viewMoreItem/ViewMoreItem.js
+++ supermarket-frontend/src/pages/viewMoreItem/ViewMoreItem.js
@@ -1,7 +1,13 @@
-import { Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import { colors, fonts } from "../../constants/palette";
 
 const ViewMoreItem = () => {
-    return <Text>View More Item</Text>
+    return <View>
+        <View>
+            <FontAwesome name="sort-amount-asc" size={fonts.iconSize} color={colors.primary} />
+        </View>
+    </View>
 }
 
 export default ViewMoreItem;
