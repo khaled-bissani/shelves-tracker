1fb71755c89d0b8cdc316b3fd6b578398d511176
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-02 15:31:20 +0200

    Frontend <> Supermarket frontend: styling the single item view page

--- supermarket-frontend/src/pages/singleItemView/SingleItemView.js
+++ supermarket-frontend/src/pages/singleItemView/SingleItemView.js
@@ -1,17 +1,19 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Text, View, Image } from "react-native";
+import { colors } from "../../constants/palette";
+import styles from "./styles";
 
 const SingleItemView = () => {
-    return <View>
-        <View>
-            <View>
-                <Image source={''}/>
+    return <View style={styles.singleItemViewContainer}>
+        <View style={styles.topContainer}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.image} source={require('../../../assets/images/landing.png')}/>
             </View>
         </View>
-        <View>
-            <View>
-                <Text>Item Name</Text>
-                <Ionicons name="create-outline"/>
+        <View style={styles.bottomContainer}>
+            <View style={styles.textContainer}>
+                <Text style={styles.text}>Item Name</Text>
+                <Ionicons name="create-outline" color={colors.primary} size={36}/>
             </View>
         </View>
     </View>
--- supermarket-frontend/src/pages/singleItemView/styles.js
+++ supermarket-frontend/src/pages/singleItemView/styles.js
@@ -2,5 +2,37 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../constants/palette";
 
 export default StyleSheet.create({
-    
+    singleItemViewContainer: {
+        flex:1
+    },
+    topContainer: {
+        justifyContent:'center',
+        alignItems:'center',
+        flex:4
+    },
+    imageContainer: {
+        width: 150,
+        height:150,
+    },
+    image: {
+        width:'100%',
+        height:'100%',
+        resizeMode:"contain"
+    },
+    bottomContainer: {
+        backgroundColor: colors.white,
+        borderTopStartRadius:50,
+        borderTopEndRadius:50,
+        flex:6.5
+    },
+    textContainer: {
+        flexDirection:'row',
+        justifyContent:'space-between',
+        alignItems:'center',
+        margin:25
+    },
+    text:{
+        fontSize: fonts.subTitle,
+        fontWeight: fonts.bold
+    },
 })
