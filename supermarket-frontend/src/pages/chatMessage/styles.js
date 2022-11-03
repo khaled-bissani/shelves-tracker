f5888ea011820bf7946cd95a8a99f7bc02d0f64f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-03 11:44:33 +0200

    Frontend <> Supermarket frontend: styling the chat box component

--- supermarket-frontend/src/pages/chatMessage/styles.js
+++ supermarket-frontend/src/pages/chatMessage/styles.js
@@ -6,31 +6,6 @@ export default StyleSheet.create({
         flex:1,
         justifyContent:'space-evenly',
     },
-    messageContainer: {
-        justifyContent:'space-between',
-        backgroundColor:colors.light_gray,
-        width:'88%',
-        padding:10,
-        marginHorizontal:25,
-        marginVertical:15
-    },
-    name: {
-        fontSize: 14
-    },
-    messageBox: {
-        flexDirection:'row',
-        flexWrap:'wrap',
-    },
-    message: {
-        fontSize: fonts.text
-    },
-    timeContainer: {
-        flexDirection:'row',
-        justifyContent:'flex-end'
-    },
-    time: {
-        fontSize: 12
-    },
     footerContainer: {
         flexDirection:'row',
         justifyContent:'space-evenly',
