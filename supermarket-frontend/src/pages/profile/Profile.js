262a9f0e7248bbdc831735ab800cfeb5f5163e8c
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-03 17:22:09 +0200

    Frontend <> Supermarket frontend: adding image picker to the change profile picture

--- supermarket-frontend/src/pages/profile/Profile.js
+++ supermarket-frontend/src/pages/profile/Profile.js
@@ -1,5 +1,7 @@
+import React, { useState } from 'react';
 import { Text, View, Image} from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import * as ImagePicker from 'expo-image-picker';
 import ProfileOption from "../../components/ProfileOption/ProfileOption";
 import styles from "./styles";
 
@@ -7,6 +9,22 @@ const Profile = () => {
 
     const navigation = useNavigation();
 
+    const [image, setImage] = useState(null);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true
+        });
+
+        if (!result.cancelled) {
+        setImage(result.base64);
+        }
+    };
+    
     return <View>
         <View style={styles.container}>
             <View style={styles.profilePictureContainer}>
@@ -17,7 +35,7 @@ const Profile = () => {
         </View>
         <View style={styles.profileOptionContainer}>
             <ProfileOption onClick={()=>navigation.navigate('EditProfile')} icon={"create-outline"} text={"Edit profile"}/>
-            <ProfileOption onClick={()=>navigation.navigate('ChangeProfilePicture')} icon={"person-circle-outline"} text={"Change profile picture"}/>
+            <ProfileOption onClick={pickImage} icon={"person-circle-outline"} text={"Change profile picture"}/>
             <ProfileOption onClick={()=>navigation.navigate('ChangePassword')} icon={"lock-closed-sharp"} text={"Change password"}/>
         </View>
     </View>
