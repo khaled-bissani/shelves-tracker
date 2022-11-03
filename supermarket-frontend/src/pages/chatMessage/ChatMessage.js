4b4058e318f186d970dd8729e9a63b49459e7040
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-03 10:18:33 +0200

    Frontend <> Supermarket frontend: adding elements to the chat message page

--- supermarket-frontend/src/pages/chatMessage/ChatMessage.js
+++ supermarket-frontend/src/pages/chatMessage/ChatMessage.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 const ChatMessage = () => {
-    return <Text>Chat Message</Text>
+    return <View>
+        <ScrollView>
+            <View>
+                <Text>Name</Text>
+                <Text>Message</Text>
+                <Text>Time</Text>
+            </View>
+        </ScrollView>
+        <View></View>
+    </View>
 }
 
 export default ChatMessage;
