96ca92e4e39b6c3a91cdc8fd332fe5bbe038d0f1
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-03 11:42:37 +0200

    Frontend <> Supermarket frontend: adding elements in the chat box component

--- supermarket-frontend/src/components/ChatBox/ChatBox.js
+++ supermarket-frontend/src/components/ChatBox/ChatBox.js
@@ -1,7 +1,16 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import styles from "./styles";
 
-const ChatBox = () => {
-    return <Text>Chat Box</Text>
+const ChatBox = (props) => {
+    return <View style={styles.messageContainer}>
+        <Text style={styles.name}>{props.name}</Text>
+        <View style={styles.messageBox}>
+            <Text style={styles.message}>{props.message}</Text>
+        </View>
+        <View style={styles.timeContainer}>
+            <Text style={styles.time}>{props.time}</Text>
+        </View>
+    </View>
 }
 
 export default ChatBox;
--- supermarket-frontend/src/pages/chatMessage/ChatMessage.js
+++ supermarket-frontend/src/pages/chatMessage/ChatMessage.js
@@ -1,21 +1,14 @@
 import { Ionicons } from "@expo/vector-icons";
 import { ScrollView, Text, TextInput, View, Pressable } from "react-native";
 import { colors, fonts } from "../../constants/palette";
+import ChatBox from "../../components/ChatBox/ChatBox";
 import styles from "./styles";
 
 const ChatMessage = () => {
     return <View style={styles.pageContainer}>
         <ScrollView>
-            <View style={styles.messageContainer}>
-                <Text style={styles.name}>Name</Text>
-                <View style={styles.messageBox}>
-                    <Text style={styles.message}>Message</Text>
-                </View>
-                <View style={styles.timeContainer}>
-                    <Text style={styles.time}>Time</Text>
-                </View>
-            </View>
-            </ScrollView>
+            <ChatBox name={'Name'} message={'Message'} time={'Time'}/>
+        </ScrollView>
         <View style={styles.footerContainer}>
             <TextInput keyboardType={'default'} style={styles.textInput}/>
             <Pressable android_ripple={{color: '#ddd'}} style={({pressed}) => pressed && styles.pressedItem}>
