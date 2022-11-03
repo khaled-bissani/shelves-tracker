0080c17f0c492c6f0116232b17e83e14663e1e9d
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-03 09:37:21 +0200

    Frontend <> Supermarket frontend: adding elements in the view more category page

--- supermarket-frontend/src/pages/viewMoreCategory/ViewMoreCategory.js
+++ supermarket-frontend/src/pages/viewMoreCategory/ViewMoreCategory.js
@@ -1,7 +1,21 @@
-import { Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
+import { ScrollView, Text, View, Pressable } from "react-native";
+import { colors, fonts } from "../../constants/palette";
+import SingleItem from "../../components/SingleItem/SingleItem";
 
 const ViewMoreCategory = () => {
-    return <Text>View More Category</Text>
+    return <View>
+        <View>
+            <Pressable>
+                <FontAwesome name="sort-amount-asc" size={fonts.iconSize} color={colors.primary} />
+            </Pressable>
+        </View>
+        <ScrollView>
+            <View>
+                <SingleItem label={"item name"} quantity={'number'} width={150} height={150}/>
+            </View>
+        </ScrollView>
+    </View>
 }
 
 export default ViewMoreCategory;
