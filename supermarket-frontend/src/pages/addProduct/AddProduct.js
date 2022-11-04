eed5b15805d7ded988e36695cad0865c23c44bf8
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-04 17:01:46 +0200

    Frontend <> Supermarket frontend: adding function for the generated barcode

--- supermarket-frontend/src/pages/addProduct/AddProduct.js
+++ supermarket-frontend/src/pages/addProduct/AddProduct.js
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { Image, Text, TextInput, View } from "react-native";
 import { colors } from "../../constants/palette";
 import Buttons from "../../components/Button/Buttons";
 import styles from "./styles";
+import Barcode from "../../components/Barcode/Barcode";
 
 const AddProduct = () => {
+
+    const [barcode, setBarcode] = useState(false);
+
+    const showBarcode = () => {
+        setBarcode(true);
+    }
+
     return <View style={styles.pageContainer}>
     <View style={styles.imageContainer}>
         <Image style={styles.image} source={''}/>
@@ -11,8 +20,9 @@ const AddProduct = () => {
     <TextInput style={styles.input} placeholder={'Product Name'}/>
     <TextInput style={styles.input} placeholder={'Product Quantity '}/>
     <TextInput style={styles.input} placeholder={'Product Expiry Date'}/>
+    {barcode ? <Barcode number={'123'}/> : null}
     <View style={styles.buttonContainer}>
-        <Buttons text={'GENERATE BARCODE'} color={colors.primary}/>
+        <Buttons text={'GENERATE BARCODE'} color={colors.primary} onClick={showBarcode}/>
         <Buttons text={'UPLOAD IMAGE'} color={colors.primary}/>
         <Buttons text={'SAVE'} color={colors.primary}/>
     </View>
