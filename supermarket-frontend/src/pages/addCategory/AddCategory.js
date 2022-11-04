9ebda9b52706e47f8e3b7604c8bc35036a94fd74
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-04 14:50:30 +0200

    Frontend <> Supermarket frontend: styling the add category page

--- supermarket-frontend/src/pages/addCategory/AddCategory.js
+++ supermarket-frontend/src/pages/addCategory/AddCategory.js
@@ -1,15 +1,18 @@
 import { Image, Text, TextInput, View } from "react-native";
 import { colors } from "../../constants/palette";
 import Buttons from "../../components/Button/Buttons";
+import styles from "./styles";
 
 const AddCategory = () => {
-    return <View>
-        <TextInput/>
-        <View>
-            <Image source={''}/>
+    return <View style={styles.pageContainer}>
+        <TextInput style={styles.input} placeholder={'Category'}/>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={''}/>
         </View>
-        <Buttons text={'UPLOAD IMAGE'} color={colors.primary}/>
-        <Buttons text={'SAVE'} color={colors.primary}/>
+        <View style={styles.buttonContainer}>
+            <Buttons text={'UPLOAD IMAGE'} color={colors.primary}/>
+            <Buttons text={'SAVE'} color={colors.primary}/>
+        </View>
     </View>
 }
 
--- supermarket-frontend/src/pages/addCategory/styles.js
+++ supermarket-frontend/src/pages/addCategory/styles.js
@@ -2,5 +2,32 @@ import { StyleSheet } from "react-native";
 import { colors, fonts } from "../../constants/palette";
 
 export default StyleSheet.create({
-    
+    pageContainer: {
+        flex:1,
+        justifyContent:'space-around',
+        alignItems:'center'
+    },
+    input: {
+        width:'75%',
+        height:50,
+        backgroundColor: colors.white,
+        fontSize: fonts.text,
+        paddingLeft:10,
+        // marginBottom:20
+    },
+    imageContainer: {
+        backgroundColor: colors.white,
+        width:250,
+        height:250,
+        borderRadius:10
+    },
+    image: {
+        width:'100%',
+        height:'100%',
+        resizeMode:"contain"
+    },
+    buttonContainer: {
+        width:'100%',
+        alignItems:'center'
+    }
 })
