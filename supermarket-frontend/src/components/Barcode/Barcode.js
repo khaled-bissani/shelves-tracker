015c67f0e053ea347c54c1f75fa0758038b4dda0
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-04 16:15:18 +0200

    Frontend <> Supermarket frontend: styling the barcode component

--- supermarket-frontend/src/components/Barcode/Barcode.js
+++ supermarket-frontend/src/components/Barcode/Barcode.js
@@ -1,9 +1,10 @@
 import {Text, View, Image } from "react-native";
+import styles from "./styles";
 
 const Barcode = (props) => {
-    return <View>
-        <Image source={require('../../../assets/images/Barcode.jpg')}/>
-        <Text>{props.number}</Text>
+    return <View style={styles.container}>
+        <Image style={styles.image} source={require('../../../assets/images/Barcode.jpg')}/>
+        <Text style={styles.text}>{props.number}</Text>
     </View>
 }
 
--- supermarket-frontend/src/components/Barcode/styles.js
+++ supermarket-frontend/src/components/Barcode/styles.js
@@ -1,5 +1,15 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    
+    container: {
+        width:150, 
+        height:50
+    },
+    image: {
+        width:'100%', 
+        height:'75%'
+    },
+    text: {
+        textAlign:'center'
+    },
 })
