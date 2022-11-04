364c6a9dbe80717c767a12c1f015744bc9ad6552
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-04 16:22:35 +0200

    Frontend <> Supermarket frontend: adding elements in the add product page

--- supermarket-frontend/src/pages/addProduct/AddProduct.js
+++ supermarket-frontend/src/pages/addProduct/AddProduct.js
@@ -1,7 +1,21 @@
-import { Text } from "react-native";
+import { Image, Text, TextInput, View } from "react-native";
+import { colors } from "../../constants/palette";
+import Buttons from "../../components/Button/Buttons";
 
 const AddProduct = () => {
-    return <Text>Add Product</Text>
+    return <View>
+    <View>
+        <Image source={''}/>
+    </View>
+    <TextInput placeholder={'Product Name'}/>
+    <TextInput placeholder={'Product Quantity '}/>
+    <TextInput placeholder={'Product Expiry Date'}/>
+    <View>
+        <Buttons text={'GENERATE BARCODE'} color={colors.primary}/>
+        <Buttons text={'UPLOAD IMAGE'} color={colors.primary}/>
+        <Buttons text={'SAVE'} color={colors.primary}/>
+    </View>
+</View>
 }
 
 export default AddProduct;
