f9749fb0b4f7a61fa351a0936098cf9cdf86b65a
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-04 17:06:14 +0200

    Frontend <> Supermarket frontend: adding image picker in the add product page

--- supermarket-frontend/src/pages/addProduct/AddProduct.js
+++ supermarket-frontend/src/pages/addProduct/AddProduct.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Image, Text, TextInput, View } from "react-native";
 import { colors } from "../../constants/palette";
+import * as ImagePicker from 'expo-image-picker';
 import Buttons from "../../components/Button/Buttons";
 import styles from "./styles";
 import Barcode from "../../components/Barcode/Barcode";
@@ -11,15 +12,30 @@ const AddProduct = () => {
 
     const [number, setNumber]=useState();
     const [barcode, setBarcode] = useState(false);
+    const [image, setImage] = useState(null);
 
     const showBarcode = () => {
         setBarcode(true);
         setNumber(RandomNumber);
     }
 
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true
+        });
+
+        if (!result.cancelled) {
+        setImage(result.uri);
+        }
+    }
+
     return <View style={styles.pageContainer}>
     <View style={styles.imageContainer}>
-        <Image style={styles.image} source={''}/>
+        <Image style={styles.image} source={{uri: image}}/>
     </View>
     <TextInput style={styles.input} placeholder={'Product Name'}/>
     <TextInput style={styles.input} placeholder={'Product Quantity '}/>
@@ -27,7 +43,7 @@ const AddProduct = () => {
     {barcode ? <Barcode number={number}/> : null}
     <View style={styles.buttonContainer}>
         <Buttons text={'GENERATE BARCODE'} color={colors.primary} onClick={showBarcode}/>
-        <Buttons text={'UPLOAD IMAGE'} color={colors.primary}/>
+        <Buttons text={'UPLOAD IMAGE'} color={colors.primary} onClick={pickImage}/>
         <Buttons text={'SAVE'} color={colors.primary}/>
     </View>
 </View>
