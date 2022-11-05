fd9260aa9d04921e2ef1d2dbfb5f05d86cd959eb
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-05 21:16:42 +0200

    Backend <> Nodejs server: adding collection in the users model

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -1,7 +1,67 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-
+    full_name: {
+        type: String,
+        required: 'full name is required'
+    },
+    username: {
+        type: String,
+        required: 'username is required'
+    },
+    email: {
+        type: String,
+        required: 'email is required'
+    },
+    password: {
+        type: String,
+        required: 'password is required'
+    },
+    phone_number: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    picture: {
+        type: String,
+    },
+    user_type: {
+        type: String,            
+        enum: ['Admin', 'Supplier', 'Supermarket'],
+    },
+    products: [{
+        product_name: {
+            type: String,
+            required: 'product name is required'
+        },
+        quantity_shelf: {
+            type: String,
+            required: 'quantity on shelf is required'
+        },
+        expiry_date: {
+            type: String,
+            required: 'expiry date is required'
+        },
+        image: {
+            type: String,
+            required: 'image is required'
+        },
+        barcode: {
+            type: String,
+            required: 'barcode is required'
+        },
+        category: {
+            category:{
+                type: String,
+                required: 'category name is required'
+            },
+            image:{
+                type: String,
+                required: 'image is required'
+            },
+        },
+    }],
 })
 
 const User = mongoose.model('User', userSchema);
