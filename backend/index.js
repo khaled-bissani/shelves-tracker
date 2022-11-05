cb8417819a59c37fa3b53c2a953f701656ac7be6
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-05 16:44:56 +0200

    Backend <> Nodejs server: adding the port number in the dotenv file

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-app.listen(3000, (err)=>{
+app.listen(process.env.PORT, (err)=>{
     if(err) console.log(err);
-    console.log(`Running server on port 3000`);
+    console.log(`Running server on port ${process.env.PORT}`);
 })
