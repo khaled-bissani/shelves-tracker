dac98ecd6e136fc3db307ceb789ef1576bd48e3e
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-06 21:36:29 +0200

    Backend <> Nodejs server: creating view profile function

--- backend/controllers/profile.controller.js
+++ backend/controllers/profile.controller.js
@@ -1,7 +1,11 @@
 const User = require('../models/users.model');
 
 const viewProfile = async(req, res) => {
+    const {id} = req.body;
 
+    const profile = await User.find({id}).select("full_name", "email", "phone_number", "location")
+
+    res.status(200).json(profile);
 }
 
 module.exports = {viewProfile};
