421102dff5ce27aecd0c290ffc796f9cf2d58d0e
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-06 14:22:25 +0200

    Backend <> Nodejs server: adding the authentication routes to the index file

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,12 @@ require('./config/database.config');
 const app = express();
 app.use(express.json());
 
+const cors = require("cors");
+app.use(cors());
+
+const authRouter = require('./routes/auth.routes');
+app.use('/auth', authRouter);
+
 app.listen(process.env.PORT, (err)=>{
     if(err) console.log(err);
     console.log(`Running server on port ${process.env.PORT}`);
