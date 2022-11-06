bc3216d2839ad9bb2109ab07c78312d779c08656
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-06 18:46:44 +0200

    Backend <> Nodejs server: creating add product function

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,7 +1,22 @@
 const User = require('../models/users.model');
 
 const addProduct = async(req,res) => {
-    
+    const {id, product_name, quantity_shelf, expiry_date, image, barcode, category} = req.body
+    try {
+        const new_product = await User.findOne({id});
+
+        new_product.products.push({
+            product_name, quantity_shelf, expiry_date, image, barcode, category
+        });
+
+        await new_product.save();
+        res.send(new_product);
+    }
+    catch(err) {
+        res.status(400).json({
+            message: err.message
+        });
+    }
 }
 
 module.exports={addProduct};
