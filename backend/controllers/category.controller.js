25a77d7000a24b5b2c9c352b68a8a8634579e251
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-06 15:29:00 +0200

    Backend <> Nodejs server: creating the add category function

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,7 +1,23 @@
 const User = require('../models/users.model');
 
-const addCategory = () => {
+const addCategory = async(req,res) => {
+    const {id, category, image} = req.body
+    try {
+        const new_category = await User.findOne({id});
 
+        new_category.products.push({
+            category:{
+                category, image
+            }});
+
+        await new_category.save();
+        res.send(new_category);
+    }
+    catch(err) {
+        res.status(400).json({
+            message: err.message
+        });
+    }
 }
 
 module.exports = {addCategory};
