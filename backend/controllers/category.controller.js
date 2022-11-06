2612838e70f85ac6f640359e00e8b3966af9b79e
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-06 22:55:10 +0200

    Backend <> Nodejs server: adding the base64 decode in the add category function

--- backend/controllers/category.controller.js
+++ backend/controllers/category.controller.js
@@ -1,13 +1,27 @@
 const User = require('../models/users.model');
+const fs = require("fs");
+const crypto = require("crypto");
 
 const addCategory = async(req,res) => {
     const {id, category, image} = req.body
+
+    const image_id = crypto.randomBytes(16).toString("hex");
+
+    var base64Data = image.replace("data:image/png;base64,", "");
+    const new_image = Buffer.from(base64Data, "base64");
+
+    fs.writeFile(__dirname.replace('controllers', 'public/images/') + image_id + ".png", new_image, 
+    (err) => {
+        console.log(err);
+    })
+
     try {
         const new_category = await User.findOne({id});
 
         new_category.products.push({
             category:{
-                category, image
+                category, 
+                image: image_id + '.png'
             }});
 
         await new_category.save();
