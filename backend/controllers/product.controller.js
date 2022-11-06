3473939d710413e2b6143ef9bd9dd68087c31944
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-06 22:58:12 +0200

    Backend <> Nodejs server: adding the base64 decode in the add product function

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,12 +1,26 @@
 const User = require('../models/users.model');
+const fs = require("fs");
+const crypto = require("crypto");
 
 const addProduct = async(req,res) => {
-    const {id, product_name, quantity_shelf, expiry_date, image, barcode, category} = req.body
+    const {id, product_name, quantity_shelf, expiry_date, image, barcode, category} = req.body;
+
+    const image_id = crypto.randomBytes(16).toString("hex");
+
+    var base64Data = image.replace("data:image/png;base64,", "");
+    const new_image = Buffer.from(base64Data, "base64");
+
+    fs.writeFile(__dirname.replace('controllers', 'public/images/') + image_id + ".png", new_image, 
+    (err) => {
+        console.log(err);
+    })
+
     try {
         const new_product = await User.findOne({id});
 
         new_product.products.push({
-            product_name, quantity_shelf, expiry_date, image, barcode, category:{category}
+            product_name, quantity_shelf, expiry_date, barcode, category:{category},
+            image: image_id + '.png'
         });
 
         await new_product.save();
