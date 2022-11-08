d24f4776ca988892ef1852d622c3f1c112ed9c5f
unknown <khaled.w.bissani@gmail.com>
2022-11-08 23:39:49 +0200

    Backend <> Nodejs server: adding change password function to the profile routes

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,9 +1,11 @@
 const {Router} = require('express');
-const {viewProfile , editProfile, profilePicture} = require('../controllers/profile.controller')
+const {viewProfile , editProfile, profilePicture, changePassword} = require('../controllers/profile.controller')
 const router = Router();
 
 router.post('/view',viewProfile);
 router.put('/edit',editProfile);
 router.post('/picture',profilePicture);
+router.put('/password',changePassword);
+
 
 module.exports= router;
