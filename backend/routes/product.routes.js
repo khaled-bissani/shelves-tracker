5cdf69525fd09fab70ec4ad0ecc629626f970cff
unknown <khaled.w.bissani@gmail.com>
2022-11-08 15:09:41 +0200

    Backend <> Nodejs server: adding the update single product function in the product routes

--- backend/routes/product.routes.js
+++ backend/routes/product.routes.js
@@ -1,9 +1,10 @@
 const {Router} = require('express');
-const {addProduct, getProduct, getSingleProduct} = require('../controllers/product.controller');
+const {addProduct, getProduct, getSingleProduct, updateSingleProduct} = require('../controllers/product.controller');
 const router = Router();
 
 router.post('/add',addProduct);
 router.get('/all', getProduct);
 router.get('/single_product', getSingleProduct);
+router.put('/single_product', updateSingleProduct)
 
 module.exports= router;
