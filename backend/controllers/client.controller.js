9c3c7222ad82d90811ee398b0c4e6ba369f3b084
unknown <khaled.w.bissani@gmail.com>
2022-11-08 22:52:33 +0200

    Backend <> Nodejs server: creating get client function

--- backend/controllers/client.controller.js
+++ backend/controllers/client.controller.js
@@ -1,7 +1,11 @@
 const User = require('../models/users.model');
 
 const getClient = async(req,res) => {
+    const {user_type} = req.body;
 
+    const user = User.find({user_type}).select('_id full_name email picture');
+
+    res.status(200).json(user);
 }
 
 module.exports={getClient};
