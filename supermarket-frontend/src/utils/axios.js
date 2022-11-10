18c4b78dd1fdf81e9de66e73c9ba913905dcfac2
unknown <khaled.w.bissani@gmail.com>
2022-11-10 05:43:16 +0200

    Frontend <> Supermarket frontend: creating get api function

--- supermarket-frontend/src/utils/axios.js
+++ supermarket-frontend/src/utils/axios.js
@@ -2,3 +2,13 @@ import axios from 'axios';
 
 axios.defaults.headers.authorization = "Bearer " + token;
 axios.defaults.baseURL = "http://localhost:3000";
+
+const getApi = async(url, data=null) => {
+    try{
+        return await axios.get(url, data);
+    }catch(error){
+        console.log("Error from GET API", error);
+    }
+}
+
+export  {getApi};
