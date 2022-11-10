48ffed954e3db3279dc4c12cee8becd9d7d26e01
unknown <khaled.w.bissani@gmail.com>
2022-11-10 11:07:14 +0200

    Frontend <> Supermarket frontend: creating formik component in the login page

--- supermarket-frontend/src/pages/login/Login.js
+++ supermarket-frontend/src/pages/login/Login.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Text, View, ScrollView } from "react-native";
+import {Formik} from "formik";
 import Buttons from "../../components/Button/Buttons";
 import GoogleButton from "../../components/GoogleButton/GoogleButton";
 import InputField from "../../components/InputField/InputField";
@@ -8,21 +9,6 @@ import styles from "./styles";
 
 const Login = ({navigation}) => {
 
-    const [user, setUser] = useState({
-        username: '',
-        password: '',
-        confirm_password: ''
-    });
-
-    const handleChange=(valueIdentifier,input)=> {
-        setUser((currentValue)=>{
-            return {
-                ...currentValue,
-                [valueIdentifier]:input
-            };
-        });
-    }
-
     const loginHandler = () => {
         navigation.navigate('Main');
     }
@@ -30,11 +16,26 @@ const Login = ({navigation}) => {
     return(
         <ScrollView contentContainerStyle={{flexGrow: 1}}>
             <View style={styles.login}>
-                <View style={styles.inputs}>
-                    <InputField type={"default"} placeholder={"Username"} value={user.username} onChange={handleChange.bind(this, 'username')}/>
-                    <InputField type={"default"} secureTextEntry={true} placeholder={"Password"} value={user.password} onChange={handleChange.bind(this, 'password')}/>
-                    <InputField type={"default"} secureTextEntry={true} placeholder={"Confirm Password"} value={user.confirm_password} onChange={handleChange.bind(this, 'confirm_password')}/>
-                </View>
+                    <Formik
+                        initialValues={{
+                            username: '',
+                            password: '',
+                            confirm_password: ''
+                        }}
+                        onSubmit={(values) => {
+
+                        }}
+                    >
+                        {(props) => (
+                            <View style={styles.inputs}>
+                                <InputField type={"default"} placeholder={"Username"} value={props.values.username} onChange={props.handleChange('username')}/>
+
+                                <InputField type={"default"} secureTextEntry={true} placeholder={"Password"} value={props.values.password} onChange={props.handleChange('password')}/>
+
+                                <InputField type={"default"} secureTextEntry={true} placeholder={"Confirm Password"} value={props.values.confirm_password} onChange={props.handleChange('confirm_password')}/>
+                            </View>
+                        )}
+                    </Formik>
                 <Buttons text={'LOGIN'} color={colors.primary} onClick={loginHandler}/>
                 <Text style={styles.text}>or</Text>
                 <GoogleButton text={'Continue with Google'}/>
