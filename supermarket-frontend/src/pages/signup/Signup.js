2145a1d35514f15501122582cced7a4ebc9c3a51
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-12 09:32:53 -0800

    Frontend <> Supermarket frontend: adding text for the validation erro in the signup page

--- supermarket-frontend/src/pages/signup/Signup.js
+++ supermarket-frontend/src/pages/signup/Signup.js
@@ -43,16 +43,22 @@ const Signup = ({navigation}) => {
                 <View style={styles.signup}>
                         <View style={styles.inputs}>
                             <InputField type={"default"} placeholder={"Full Name"} value={props.values.full_name} onChange={props.handleChange('full_name')} onBlur={props.handleBlur('full_name')}/>
+                            <Text style={styles.errorText}>{props.touched.full_name && props.errors.full_name}</Text>
 
                             <InputField type={"default"} placeholder={"Username"} value={props.values.username} onChange={props.handleChange('username')} onBlur={props.handleBlur('username')}/>
+                            <Text style={styles.errorText}>{props.touched.username && props.errors.username}</Text>
 
                             <InputField type={"email-address"} placeholder={"Email"} value={props.values.email} onChange={props.handleChange('email')} onBlur={props.handleBlur('email')}/>
+                            <Text style={styles.errorText}>{props.touched.email && props.errors.email}</Text>
 
                             <InputField type={"number-pad"} placeholder={"Phone Number"} value={props.values.phone_number} onChange={props.handleChange('phone_number')} onBlur={props.handleBlur('phone_number')}/>
+                            <Text style={styles.errorText}>{props.touched.phone_number && props.errors.phone_number}</Text>
 
                             <InputField type={"default"} secureTextEntry={true} placeholder={"Password"} value={props.values.password} onChange={props.handleChange('password')} onBlur={props.handleBlur('password')}/>
+                            <Text style={styles.errorText}>{props.touched.password && props.errors.password}</Text>
 
                             <InputField type={"default"} secureTextEntry={true} placeholder={"Confirm Password"} value={props.values.confirm_password} onChange={props.handleChange('confirm_password')} onBlur={props.handleBlur('confirm_password')}/>
+                            <Text style={styles.errorText}>{props.touched.confirm_password && props.errors.confirm_password}</Text>
                         </View>
                         <Buttons text={'CREATE ACCOUNT'} color={colors.primary} onClick={props.handleSubmit}/>
                     <View style={styles.textContainer}>
