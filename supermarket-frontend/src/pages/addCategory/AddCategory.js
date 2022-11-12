4900db52b5d0f425642af70075c701cfce123b74
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-12 11:33:33 -0800

    Frontend <> Supermarket frontend: adding formik in the add category page

--- supermarket-frontend/src/pages/addCategory/AddCategory.js
+++ supermarket-frontend/src/pages/addCategory/AddCategory.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Image, Text, TextInput, View } from "react-native";
+import { Image, Text, TextInput, View, ScrollView } from "react-native";
 import { colors } from "../../constants/palette";
 import * as ImagePicker from 'expo-image-picker';
 import Buttons from "../../components/Button/Buttons";
 import styles from "./styles";
+import { Formik } from 'formik';
 
 const AddCategory = () => {
 
@@ -25,16 +26,27 @@ const AddCategory = () => {
         }
     };
 
-    return <View style={styles.pageContainer}>
-        <View style={styles.imageContainer}>
-            <Image style={styles.image} source={{uri: image}}/>
-        </View>
-        <TextInput style={styles.input} placeholder={'Category'}/>
-        <View style={styles.buttonContainer}>
-            <Buttons text={'UPLOAD IMAGE'} color={colors.primary} onClick={pickImage}/>
-            <Buttons text={'SAVE'} color={colors.primary}/>
-        </View>
-    </View>
+    return <ScrollView contentContainerStyle={{flexGrow: 1}}>
+        <Formik 
+        initialValues={{
+            category:'',
+            image: base64Image
+        }}
+        >
+            {(props) => (
+                <View style={styles.pageContainer}>
+                    <View style={styles.imageContainer}>
+                        <Image style={styles.image} source={{uri: image}}/>
+                    </View>
+                    <TextInput style={styles.input} placeholder={'Category'}/>
+                    <View style={styles.buttonContainer}>
+                        <Buttons text={'UPLOAD IMAGE'} color={colors.primary} onClick={pickImage}/>
+                        <Buttons text={'SAVE'} color={colors.primary}/>
+                    </View>
+                </View>
+            )}
+        </Formik>
+    </ScrollView>
 }
 
 export default AddCategory;
