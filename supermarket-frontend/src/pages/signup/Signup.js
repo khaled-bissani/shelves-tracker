78073ec6aa89f8d59b6984af3a7556a5c892bdb1
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-12 09:24:15 -0800

    Frontend <> Supermarket frontend: adding validation to the form in the signup page

--- supermarket-frontend/src/pages/signup/Signup.js
+++ supermarket-frontend/src/pages/signup/Signup.js
@@ -1,11 +1,21 @@
 import { Text, View, ScrollView } from "react-native";
 import {Formik} from "formik";
+import * as yup from "yup";
 import Buttons from "../../components/Button/Buttons";
 import GoogleButton from "../../components/GoogleButton/GoogleButton";
 import InputField from "../../components/InputField/InputField";
 import PressableText from "../../components/PressableText/PressableText";
 import { colors, fonts } from "../../constants/palette";
 import styles from "./styles";
+
+const userSchema = yup.object({
+    full_name: yup.string().required("Full name is required").min(6),
+    username: yup.string().required().min(6),
+    email: yup.string().email("Field should contain a valid email").max(255).required("Email is required"),
+    phone_number: yup.number().min(8).required("Phone number is required"),
+    password: yup.string().required("Password is required").min(8),
+    confirm_password: yup.string().required("Confirm password is required").oneOf([yup.ref('password'), null], 'Passwords must match'),
+})
 
 const Signup = ({navigation}) => {
 
@@ -24,6 +34,7 @@ const Signup = ({navigation}) => {
                 password: '',
                 confirm_password: ''
             }}
+            validationSchema={userSchema}
             onSubmit={(values, actions) => {
                 console.log('submitted')
             }}
