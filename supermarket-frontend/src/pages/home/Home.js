1d06712aad5a4146c1f1b3709f78f4f352cfe641
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-13 09:03:39 -0800

    Frontend <> Supermarket frontend: fetching all the categories in the home page

--- supermarket-frontend/src/pages/home/Home.js
+++ supermarket-frontend/src/pages/home/Home.js
@@ -1,13 +1,30 @@
+import { useState, useEffect } from "react";
 import { Text, View, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import styles from "./styles";
 import Categories from "../../components/Categories/Categories";
 import PressableText from "../../components/PressableText/PressableText";
 import SingleCategory from "../../components/SingleCategory/SingleCategory";
+import sendRequest from "../../utils/axios";
+import baseUrl from "../../../config/env";
 
 const Home = () => {
 
+    const [categories, setCategories] = useState()
+
     const navigation = useNavigation();
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                const allCategory = await sendRequest({method:"get",data:"636ef8497eb94fe486471d25",route:`${baseUrl.BASE_URL}/category/all`})
+                setCategories(allCategory)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])
 
     return <>
     <View style={styles.homeContainer}>
