c64cc1f237a179cddcecef6a871c19c630bf2e90
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-12 22:44:37 -0800

    Frontend <> Supermarket frontend: styling the add input field component

--- supermarket-frontend/src/components/AddInputField/AddInputField.js
+++ supermarket-frontend/src/components/AddInputField/AddInputField.js
@@ -1,7 +1,8 @@
 import { TextInput } from "react-native";
+import styles from "./styles";
 
 const AddInputField = () => {
-    return <TextInput placeholder={props.placeholder} value={props.value} onChangeText={props.onChange} onBlur={props.onBlur}/>
+    return <TextInput placeholder={props.placeholder} value={props.value} onChangeText={props.onChange} onBlur={props.onBlur} style={styles.inputContainer}/>
 }
 
 export default AddInputField;
--- supermarket-frontend/src/components/AddInputField/styles.js
+++ supermarket-frontend/src/components/AddInputField/styles.js
@@ -1,5 +1,13 @@
 import { StyleSheet } from "react-native";
+import { colors, fonts } from "../../constants/palette";
 
 export default StyleSheet.create({
-    
+    inputContainer: {
+        width:'75%',
+        height:40,
+        backgroundColor: colors.white,
+        fontSize: fonts.text,
+        paddingLeft:10,
+        marginBottom:20
+    }
 })
