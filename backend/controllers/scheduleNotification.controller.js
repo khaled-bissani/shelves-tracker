521380631bd8e83196b25d7e0df99294e7d97c4f
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-16 23:47:02 +0200

    Backend <> Nodejs server: creating the schedule function

--- backend/controllers/scheduleNotification.controller.js
+++ backend/controllers/scheduleNotification.controller.js
@@ -10,5 +10,37 @@ const notification = async(expoPushToken, message) => {
 }
 
 const schedule = async(req, res) => {
+    const allUser = await User.find()
 
+    // Getting the id and the expo token from the database
+    for (let i = 0; i < allUser.length; i++) {
+        const id = allUser[i]._id.toString();
+        const expoPushToken = allUser[i].expoPushToken
+
+        const user = await User.findById(id)
+    
+        // Filtering the categories to access the array of products
+        const filteredCategories = user.category.filter((singleCategory)=>{
+            return singleCategory.products
+        })
+    
+        // Getting an array of all the products available
+        const allProducts = [];
+        for(let i=0; i<filteredCategories.length; i++){
+            allProducts.push(...filteredCategories[i].products) 
+        }
+
+        // Getting the current day
+        const today = new Date();
+        
+        // Accessing the array of products to check if the expiry date of each product is within a week
+        for (let i = 0; i < allProducts.length; i++) {
+            const singleProduct = allProducts[i];
+            const remainingDay = new Date(singleProduct.expiry_date).getDate()-today.getDate()
+            if(remainingDay < 7){
+                const message = `${singleProduct.product_name} is expiring in less than a week. ${remainingDay} days left`;
+                notification(expoPushToken, message)
+            }
+        }
+    }
 }
