dab333950c6a92f9a54042250c370c7add942e2a
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-15 21:15:35 -0800

    Backend <> Nodejs server: editing the category routes

--- backend/routes/category.routes.js
+++ backend/routes/category.routes.js
@@ -3,6 +3,6 @@ const { addCategory, getCategory } = require('../controllers/category.controller
 const router = Router();
 
 router.post('/add',addCategory);
-router.get('/all', getCategory)
+router.post('/all', getCategory)
 
 module.exports= router;
