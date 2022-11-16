6eae16b6e22065547bbe2ec8c2f6aa0d173285d7
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-16 23:44:43 +0200

    Backend <> Nodejs server: creating schedule notification controller

---
+++ backend/controllers/scheduleNotification.controller.js
@@ -0,0 +1,8 @@
+const cron = require('node-cron');
+const User = require('../models/users.model');
+const { Expo } = require('expo-server-sdk');
+const sendPushNotification = require('../utilities/pushNotifications');
+
+const schedule = async(req, res) => {
+    
+}
