baca916ad48fbac4b0e5adbead4e0823e21042ae
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-17 00:54:13 +0200

    Frontend <> Supermarket frontend: adding notification handler to show the push notification

--- supermarket-frontend/src/pages/notification/Notification.js
+++ supermarket-frontend/src/pages/notification/Notification.js
@@ -1,5 +1,18 @@
+import { useState, useEffect, useRef } from 'react'
 import { ScrollView} from "react-native";
 import SingleNotification from "../../components/SingleNotification/SingleNotification";
+import * as Notifications from 'expo-notifications';
+import sendRequest from "../../utils/axios";
+import baseUrl from "../../../config/env";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: false,
+    }),
+  });
 
 const Notification = () => {
     return <ScrollView>
