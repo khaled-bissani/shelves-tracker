3e83c68ecbfb9286c1019223286618cad00eb236
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-16 23:46:18 +0200

    Backend <> Nodejs server: creating notification function to call the send push notification

--- backend/controllers/scheduleNotification.controller.js
+++ backend/controllers/scheduleNotification.controller.js
@@ -3,6 +3,12 @@ const User = require('../models/users.model');
 const { Expo } = require('expo-server-sdk');
 const sendPushNotification = require('../utilities/pushNotifications');
 
+const notification = async(expoPushToken, message) => {
+    if (Expo.isExpoPushToken(expoPushToken)) {
+        await sendPushNotification(expoPushToken, message);
+    }
+}
+
 const schedule = async(req, res) => {
-    
+
 }
