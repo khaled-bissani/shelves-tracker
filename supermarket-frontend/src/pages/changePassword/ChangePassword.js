791343f98ff973165db798a8ac4b9c781af1c2c9
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-17 03:16:07 +0200

    Frontend <> Supermarket frontend: adding formik for the change password page

--- supermarket-frontend/src/pages/changePassword/ChangePassword.js
+++ supermarket-frontend/src/pages/changePassword/ChangePassword.js
@@ -3,16 +3,28 @@ import { colors } from "../../constants/palette";
 import InputField from "../../components/InputField/InputField";
 import Buttons from "../../components/Button/Buttons";
 import styles from "./styles";
+import {Formik} from "formik";
 
 const ChangePassword = () => {
-    return <View style={styles.changePasswordContainer}>
-    <View style={styles.inputs}>
-        <InputField type={"default"} secureTextEntry={true} placeholder={"Old Password"}/>
-        <InputField type={"default"} secureTextEntry={true} placeholder={"New Password"}/>
-        <InputField type={"default"} secureTextEntry={true} placeholder={"Confirm Password"}/>
-    </View>
-    <Buttons text={'SAVE'} color={colors.primary}/>
-</View>
+    return <Formik 
+    initialValues={{
+        old_password:'',
+        new:'',
+        confirm_password:'',
+    }}
+    >
+        {(props) => (
+        <View style={styles.changePasswordContainer}>
+            <View style={styles.inputs}>
+                <InputField type={"default"} secureTextEntry={true} placeholder={"Old Password"}/>
+                <InputField type={"default"} secureTextEntry={true} placeholder={"New Password"}/>
+                <InputField type={"default"} secureTextEntry={true} placeholder={"Confirm Password"}/>
+            </View>
+            <Buttons text={'SAVE'} color={colors.primary}/>
+        </View>
+
+        )}
+    </Formik>
 }
 
 export default ChangePassword;
