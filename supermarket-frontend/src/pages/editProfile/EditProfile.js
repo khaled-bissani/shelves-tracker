a70297c4291d346036a5d763011b1ba524fe2985
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-17 02:25:15 +0200

    Frontend <> Supermarket frontend: fetching the view profile api

--- supermarket-frontend/src/pages/editProfile/EditProfile.js
+++ supermarket-frontend/src/pages/editProfile/EditProfile.js
@@ -1,13 +1,36 @@
+import React, { useState, useEffect } from 'react';
 import { View, Image } from "react-native";
 import { colors } from "../../constants/palette";
 import Buttons from "../../components/Button/Buttons";
 import InputField from "../../components/InputField/InputField";
 import styles from "./styles";
+import sendRequest from '../../utils/axios';
+import baseUrl from '../../../config/env';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const EditProfile = ({route}) => {
 
     const profilePicture=route.params.profilePicture
 
+    const [userId, setUserId] = useState("");
+
+    AsyncStorage.getItem('userId').then((value)=> setUserId(value));
+
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                await sendRequest({method:"post",data:userId,route:`${baseUrl.BASE_URL}/profile/view`})
+                .then((res)=>{
+                console.log(res)})
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        if(userId.length>0){
+            fetchData()
+        }
+    }, [userId])
+    
     return <View style={styles.editProfileContainer}>
         <View style={styles.imageContainer}>
             <Image style={styles.image} source={{uri : profilePicture}}/>
