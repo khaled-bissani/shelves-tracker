8a524b96f807c947bbf0976312d8c5226a7b7305
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-17 03:51:21 +0200

    Backend <> Nodejs server: adding the view profile picture to the profile routes

--- backend/routes/profile.routes.js
+++ backend/routes/profile.routes.js
@@ -1,10 +1,11 @@
 const {Router} = require('express');
-const {viewProfile , editProfile, profilePicture, changePassword} = require('../controllers/profile.controller')
+const {viewProfile , editProfile, profilePicture, viewProfilePicture, changePassword} = require('../controllers/profile.controller')
 const router = Router();
 
 router.post('/view',viewProfile);
 router.put('/edit',editProfile);
 router.post('/picture',profilePicture);
+router.post('/view_picture', viewProfilePicture)
 router.put('/password',changePassword);
 
 
