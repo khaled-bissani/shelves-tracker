1442f25059225041c1f857c69ca350dc30cace65
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 06:23:09 +0200

    Frontend <> Supplier frontend: adding formik in the login page

--- supplier-frontend/src/pages/login/Login.js
+++ supplier-frontend/src/pages/login/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom"
 import SignupLoginButton from '../../components/SignupLoginButton'
 import TextInput from '../../components/TextInput'
 import Background from '../../assets/background.jpg'
+import {Formik} from 'formik'
 
 const Login = () => {
 
@@ -25,20 +26,30 @@ const Login = () => {
     }
 
   return (
-    <div className='flex justify-end h-screen' style={{backgroundImage : `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
-        <div className='flex flex-col items-center justify-around w-[375px] h-[450px] bg-[#3AA346] mt-28 mr-56'>
-            <h1 className='font-sans font-bold text-[24px] text-white'>Login</h1>
-            <TextInput label={"Email"} type={"email"} placeholder={"Email"} value={user.email} onChange={(e)=>handleChange(e,"email")} textColor="white" bgColor="white"/>
-            <TextInput label={"Password"} type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")} textColor="white" bgColor="white"/>
-            <TextInput label={"Current Password"} type={"password"} placeholder={"Current Password"} value={user.current_password} onChange={(e)=>handleChange(e,"current_password")} textColor="white" bgColor="white"/>
-            <SignupLoginButton text={"LOGIN"} onClick={handleClick}/>
-            <p className='font-sans'>Don't have an account?
-                <span className='text-white cursor-pointer' onClick={() =>{navigate("/")}}>
-                     Signup
-                </span>
-            </p>
-        </div>
-    </div>
+    <Formik
+    initialValues={{
+        username:'',
+        password:'',
+        confirm_password:''
+    }}
+    >
+        {(props)=> (
+            <div className='flex justify-end h-screen' style={{backgroundImage : `url(${Background})`, backgroundRepeat:"no-repeat", backgroundSize:"cover"}}>
+                <div className='flex flex-col items-center justify-around w-[375px] h-[450px] bg-[#3AA346] mt-28 mr-56'>
+                    <h1 className='font-sans font-bold text-[24px] text-white'>Login</h1>
+                    <TextInput label={"Username"} type={"text"} placeholder={"Username"} value={user.username} onChange={(e)=>handleChange(e,"username")} textColor="white" bgColor="white"/>
+                    <TextInput label={"Password"} type={"password"} placeholder={"Password"} value={user.password} onChange={(e)=>handleChange(e,"password")} textColor="white" bgColor="white"/>
+                    <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} value={user.current_password} onChange={(e)=>handleChange(e,"current_password")} textColor="white" bgColor="white"/>
+                    <SignupLoginButton text={"LOGIN"} onClick={handleClick}/>
+                    <p className='font-sans'>Don't have an account?
+                        <span className='text-white cursor-pointer' onClick={() =>{navigate("/")}}>
+                            Signup
+                        </span>
+                    </p>
+                </div>
+            </div>
+        )}
+    </Formik>
   )
 }
 
