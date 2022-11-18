e21f6466e62a7d5a7bb7c6db44e7b2611f788a03
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 12:38:36 +0200

    Frontend <> Supplier frontend: adding use state to navigte between the edit profile and the change password

--- supplier-frontend/src/pages/home/Profile.js
+++ supplier-frontend/src/pages/home/Profile.js
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react'
 import ProfileImage from '../../components/ProfileImage'
 import ProfileInformation from '../../components/ProfileInformation'
-import ProfileOption from '../../components/ProfileOption'
 import SideBar from '../../components/SideBar'
 import Title from '../../components/Title'
 import {FaEdit,FaRegUserCircle, FaLock} from "react-icons/fa";
+import EditProfile from '../../components/EditProfile'
+import ChangePassword from '../../components/ChangePassword'
 
 const Profile = () => {
   const buttonClass=`flex text-black text-[16px] font-sans`
   const iconClass={color: "#3AA346", fontSize:"25px", marginRight:"10px"}
+
+  const [clicked, setClicked] = useState({
+    edit:false,
+    password:false
+  })
+
+  const handleEditClick = () => {
+    setClicked({
+      edit:true,
+      password:false
+    })
+  }
+
+  const handlePasswordClick = () => {
+    setClicked({
+      edit:false,
+      password:true
+    })
+  }
 
   return (
     <div className='flex'>
@@ -19,7 +39,7 @@ const Profile = () => {
                 <div className='flex flex-col justify-between my-6 mr-6'>
                     <ProfileImage/>
                     <div className='flex flex-col items-start justify-around pl-8 w-[400px] h-[175px] bg-white rounded-[10px]'>
-                        <button className={buttonClass}>
+                        <button className={buttonClass} onClick={handleEditClick}>
                             <FaEdit style={iconClass}/>
                             <p>Edit profile</p>
                         </button>
@@ -27,13 +47,15 @@ const Profile = () => {
                             <FaRegUserCircle style={iconClass}/>
                             <p>Change profile picture</p>
                         </button>
-                        <button className={buttonClass}>
+                        <button className={buttonClass} onClick={handlePasswordClick}>
                             <FaLock style={iconClass}/>
                             <p>Change password</p>
                         </button>
                     </div>
                 </div>
-                <ProfileInformation/>
+                {clicked.edit===false && clicked.password===false ? <ProfileInformation/> : null}
+                {clicked.edit ? <EditProfile/> :null  }
+                {clicked.password ? <ChangePassword/> :null }
             </div>
         </div>
     </div>
