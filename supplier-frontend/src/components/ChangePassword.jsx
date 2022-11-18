8ff116f662c8c78ae12a71529edf2b0ee74d3371
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 13:14:44 +0200

    Frontend <> Supplier frontend: styling the validation error message

--- supplier-frontend/src/components/ChangePassword.jsx
+++ supplier-frontend/src/components/ChangePassword.jsx
@@ -28,8 +28,14 @@ const ChangePassword = () => {
       {(props)=>(
         <div className='flex flex-col items-center justify-around w-[400px] h-[370px] bg-white rounded-[10px] my-6'>
             <TextInput label={"Old Password"} type={"password"} placeholder={"Old Password"} textColor="black" bgColor="#D9D9D9" value={props.values.old_password} onChange={props.handleChange("old_password")}/>
+            <p className='font-bold text-[14px] text-[#ff0000]'>{props.errors.old_password}</p>
+
             <TextInput label={"New Password"} type={"password"} placeholder={"New Password"} textColor="black" bgColor="#D9D9D9" value={props.values.new_password} onChange={props.handleChange("new_password")}/>
+            <p className='font-bold text-[14px] text-[#ff0000]'>{props.errors.new_password}</p>
+
             <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} textColor="black" bgColor="#D9D9D9" value={props.values.confirm_password} onChange={props.handleChange("confirm_password")}/>
+            <p className='font-bold text-[14px] text-[#ff0000]'>{props.errors.confirm_password}</p>
+
             <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"} onClick={props.handleSubmit}/>
         </div>
       )}
