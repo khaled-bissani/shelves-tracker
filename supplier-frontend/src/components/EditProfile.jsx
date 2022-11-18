fba6b3c0dfc00ff3db5ec5d6bd6733b1493b486e
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 09:12:52 +0200

    Frontend <> Supplier frontend: linking the edit profile with the edit profile api

--- supplier-frontend/src/components/EditProfile.jsx
+++ supplier-frontend/src/components/EditProfile.jsx
@@ -1,16 +1,29 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Button from './Button'
 import TextInput from './TextInput'
+import sendRequest from '../utils/axios'
 
 
 const EditProfile = () => {
 
+  const user = JSON.parse(localStorage.getItem("userInfo"))
+  const userId = localStorage.getItem("userId")
+
+  const [updated, setUpdated] = useState(false)
+
   const[profile,setProfile]=useState({
-    name: '',
-    email: '',
-    phone_number: '',
-    address: ''
-  })
+    id:userId,
+    name: user.full_name,
+    email: user.email,
+    phone_number: user.phone_number,
+    address: user.address
+  }) 
+
+  useEffect(() => {
+    console.log(updated)
+    setUpdated(true)
+  }, [profile])
+  
 
   const handleChange=(e,input)=> {
     setProfile({
@@ -18,8 +31,14 @@ const EditProfile = () => {
     });
   }
 
-  const handleClick = () => {
-    console.log('save')
+  const editProfile = () => {
+    sendRequest({method:"put",data:profile,route:`${process.env.REACT_APP_BASE_URL}/profile/edit`})
+    .then((res)=>{
+        console.log(res)
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
   }
 
   return (
@@ -28,7 +47,7 @@ const EditProfile = () => {
         <TextInput label={"Email"} type={"text"} placeholder={"Email"} textColor="black" bgColor="#D9D9D9" value={profile.email} onChange={(e)=>handleChange(e,"email")}/>
         <TextInput label={"Phone Number"} type={"text"} placeholder={"Phone Number"} textColor="black" bgColor="#D9D9D9" value={profile.phone_number} onChange={(e)=>handleChange(e,"phone_number")}/>
         <TextInput label={"Address"} type={"text"} placeholder={"Address"} textColor="black" bgColor="#D9D9D9" value={profile.address} onChange={(e)=>handleChange(e,"address")}/>
-        <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"} onClick={handleClick}/>
+        {updated? <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"} onClick={editProfile}/> : <Button bgColor="#808080" width="300px" height="40px" name={"SAVE"} />}
     </div>
   )
 }
