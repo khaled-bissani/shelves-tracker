8c5bc458d8ab7f81a5cf1c85f96f59e9c6be0f1b
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 09:34:28 +0200

    Frontend <> Supplier frontend: deleting the profile option component

--- supplier-frontend/src/pages/home/Profile.js
+++ supplier-frontend/src/pages/home/Profile.js
@@ -4,8 +4,12 @@ import ProfileInformation from '../../components/ProfileInformation'
 import ProfileOption from '../../components/ProfileOption'
 import SideBar from '../../components/SideBar'
 import Title from '../../components/Title'
+import {FaEdit,FaRegUserCircle, FaLock} from "react-icons/fa";
 
 const Profile = () => {
+  const buttonClass=`flex text-black text-[16px] font-sans`
+  const iconClass={color: "#3AA346", fontSize:"25px", marginRight:"10px"}
+
   return (
     <div className='flex'>
         <SideBar/>
@@ -14,7 +18,20 @@ const Profile = () => {
             <div className='flex justify-center w-[900px] h-[420px] bg-[#D9D9D9] rounded-[10px]'>
                 <div className='flex flex-col justify-between my-6 mr-6'>
                     <ProfileImage/>
-                    <ProfileOption/>
+                    <div className='flex flex-col items-start justify-around pl-8 w-[400px] h-[175px] bg-white rounded-[10px]'>
+                        <button className={buttonClass}>
+                            <FaEdit style={iconClass}/>
+                            <p>Edit profile</p>
+                        </button>
+                        <button className={buttonClass}>
+                            <FaRegUserCircle style={iconClass}/>
+                            <p>Change profile picture</p>
+                        </button>
+                        <button className={buttonClass}>
+                            <FaLock style={iconClass}/>
+                            <p>Change password</p>
+                        </button>
+                    </div>
                 </div>
                 <ProfileInformation/>
             </div>
