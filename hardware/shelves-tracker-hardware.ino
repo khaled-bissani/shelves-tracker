4c0908df35e0343052c86514fd8cb7f6fbfedf0d
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 17:41:31 +0200

    Hardware <> Arduino: adding the weight sensor reading to the arduino

--- hardware/shelves-tracker-hardware.ino
+++ hardware/shelves-tracker-hardware.ino
@@ -1,6 +1,12 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
+#include "HX711.h"
+
+const int LOADCELL_DOUT_PIN = 12;
+const int LOADCELL_SCK_PIN = 13;
+
+HX711 scale;
 
 const char* ssid = "ssid";
 const char* password = "password";
@@ -30,11 +36,57 @@ void setup() {
   Serial.println(WiFi.localIP());
  
   Serial.println("Timer set to 1 hour (timerDelay variable), it will take an hour before publishing the first reading.");
+
+  //HX711 Weight sensor 
+  Serial.println("Initializing the scale");
+
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);
+            
+  scale.set_scale(1.6);
+  scale.tare();          
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read()); 
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));   
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);
+  
+  Serial.println("Readings:");
 }
 
 void loop() {
 
-if ((millis() - lastTime) > timerDelay) {
+  if ((millis() - lastTime) > timerDelay) {
+
+    Serial.print("one reading:\t");
+    Serial.print(scale.get_units(), 1);
+    Serial.print("\t| average:\t");
+    Serial.println(scale.get_units(10), 5);
+
+    scale.power_down(); 
+    delay(5000);
+    scale.power_up();
 
     if(WiFi.status()== WL_CONNECTED){
       WiFiClient client;
@@ -55,5 +107,5 @@ if ((millis() - lastTime) > timerDelay) {
     }
     lastTime = millis();
   }
-  
+
 }
