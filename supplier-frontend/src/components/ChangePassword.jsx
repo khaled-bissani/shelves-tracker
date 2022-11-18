c5c5b6f9cde321f4b75ddf9469a83cce980dc3a1
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-18 13:06:08 +0200

    Frontend <> Supplier frontend: adding fromik in the change password component

--- supplier-frontend/src/components/ChangePassword.jsx
+++ supplier-frontend/src/components/ChangePassword.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import Button from './Button'
 import TextInput from './TextInput'
+import {Formik} from 'formik'
 
 const ChangePassword = () => {
 
@@ -21,13 +22,16 @@ const ChangePassword = () => {
   }
 
   return (
-    <div className='flex flex-col items-center justify-around w-[400px] h-[370px] bg-white rounded-[10px] my-6'>
-        <TextInput label={"Old Password"} type={"password"} placeholder={"Old Password"} textColor="black" bgColor="#D9D9D9" value={password.old_password} onChange={(e)=>handleChange(e,"old_password")}/>
-        <TextInput label={"New Password"} type={"password"} placeholder={"New Password"} textColor="black" bgColor="#D9D9D9" value={password.new_password} onChange={(e)=>handleChange(e,"new_password")}/>
-        <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} textColor="black" bgColor="#D9D9D9" value={password.confirm_password} onChange={(e)=>handleChange(e,"confirm_password")}/>
-        <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"} onClick={handleClick}/>
-    </div>
-  )
+    <Formik>
+      {(props)=>(
+        <div className='flex flex-col items-center justify-around w-[400px] h-[370px] bg-white rounded-[10px] my-6'>
+            <TextInput label={"Old Password"} type={"password"} placeholder={"Old Password"} textColor="black" bgColor="#D9D9D9" value={password.old_password} onChange={(e)=>handleChange(e,"old_password")}/>
+            <TextInput label={"New Password"} type={"password"} placeholder={"New Password"} textColor="black" bgColor="#D9D9D9" value={password.new_password} onChange={(e)=>handleChange(e,"new_password")}/>
+            <TextInput label={"Confirm Password"} type={"password"} placeholder={"Confirm Password"} textColor="black" bgColor="#D9D9D9" value={password.confirm_password} onChange={(e)=>handleChange(e,"confirm_password")}/>
+            <Button bgColor="#3AA346" width="300px" height="40px" name={"SAVE"} onClick={handleClick}/>
+        </div>
+      )}
+    </Formik>)
 }
 
 export default ChangePassword
