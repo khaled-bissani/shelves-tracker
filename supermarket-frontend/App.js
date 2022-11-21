c45be4562bfa5f8e0b9f35b13c3e26a7660849d7
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-21 16:38:50 +0200

    Frontend <> Supermarket frontend: removing the style from the app.js

--- supermarket-frontend/App.js
+++ supermarket-frontend/App.js
@@ -1,4 +1,3 @@
-import {StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import MainStackNavigation from './src/navigation/MainStackNavigation';
 
@@ -11,9 +10,3 @@ export default function App() {
     </>    
   );
 }
-
-const styles= StyleSheet.create({
-  mainContainer: {
-    flex:1, 
-  }
-})
