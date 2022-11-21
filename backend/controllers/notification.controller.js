f599022847226ab072e5f942545d749cfde8cd67
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-21 14:59:39 +0200

    Backend <> Nodejs server: creating save notification function

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -3,10 +3,28 @@ const User = require('../models/users.model');
 const expoToken = async(req,res) => {
   const {id, expoPushToken} = req.body;
 
-  const user = await User.findByIdAndUpdate(id, {expoPushToken})
-
-  user.save()
-  res.status(200).json({message: "Expo Token updated successfully"})
+  await User.findByIdAndUpdate(id, {expoPushToken})
+  .then(res.status(200).json({message: "Expo Token updated successfully"}))
+  .catch((err)=>res.status(400).send(err))  
+  
 }
 
-module.exports={expoToken};
+const saveNotification = async(req,res) => {
+  const {id, single_notification} = req.body;
+
+  try {
+    const user = await User.findById(id);
+
+    user.notifications.push({
+      single_notification
+      });
+
+  await user.save();
+  res.send(user.notifications);
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports={expoToken, saveNotification};
