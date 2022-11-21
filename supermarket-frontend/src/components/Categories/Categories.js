881e4642b7b1d80d34d76a26241462bfcb16bee1
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-21 16:46:50 +0200

    Frontend <> Supermarket frontend: adding dotenv variable for the image

--- supermarket-frontend/src/components/Categories/Categories.js
+++ supermarket-frontend/src/components/Categories/Categories.js
@@ -1,8 +1,9 @@
 import { Text, View, Pressable, Image } from "react-native";
 import styles from "./styles";
+import baseUrl from "../../../config/env";
 
 const Categories = (props) => {
-    const image = `http://192.168.44.144:3000/static/images/${props.imageUrl}`
+    const image = `${baseUrl.BASE_URL}/static/images/${props.imageUrl}`
     return (
         <>
             <Pressable onPress={props.onClick} android_ripple={{color: '#ddd'}} style={({pressed}) => pressed && styles.pressedItem}>
