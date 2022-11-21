e13e73f633d3afc294b5a81021424e2d95fbaadc
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-21 13:59:34 +0200

    Frontend <> Supplier frontend: installing the recharts

--- supplier-frontend/src/pages/home/Home.js
+++ supplier-frontend/src/pages/home/Home.js
@@ -8,14 +8,10 @@ const Home = () => {
       <SideBar/>
       <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
         <Title title={"Dashboard"}/>
-        <div className='w-[900px] h-[100px] bg-[#D9D9D9]'></div>
-        <div className='flex justify-between'>
-          <div className='w-[575px] h-[300px] bg-[#D9D9D9]'></div>
-          <div className='flex flex-col justify-between'>
-            <div className='w-[300px] h-[140px] bg-[#D9D9D9]'></div>
-            <div className='w-[300px] h-[140px] bg-[#D9D9D9]'></div>
+        <div className='w-[900px] h-[120px] bg-[#D9D9D9]'></div>
+          <div className='flex-col justify-center items-center w-[900px] h-[280px] bg-[#D9D9D9]'>
+            <p className='text-center'>Waste products per Week</p>
           </div>
-        </div>
       </div>
     </div>
   )
