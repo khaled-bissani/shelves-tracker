83a023d74a43dcf5aed20c5f99ba354843e70a55
khaled-bissani <khaled.w.bissani@gmail.com>
2022-11-24 14:23:50 +0200

    Frontend <> Supplier frontend: linking the show product api in the table

--- supplier-frontend/src/pages/home/Expiry.js
+++ supplier-frontend/src/pages/home/Expiry.js
@@ -7,6 +7,7 @@ import Title from '../../components/Title'
 import Popup from 'reactjs-popup'
 import TextInput from '../../components/TextInput'
 import sendRequest from '../../utils/axios'
+import ExpiryRow from '../../components/ExpiryRow'
 
 const Expiry = () => {
 
@@ -43,30 +44,46 @@ const Expiry = () => {
         });
       }
 
-    const handleAdd = () =>{
-        console.log('Add')
+    const handleDelete = async() =>{
+        try {
+            const deleteProduct = await sendRequest({method:"post" ,data:{"id":userId,"category_id":categoryId,"product_name":selectedRow} ,route:`${process.env.REACT_APP_BASE_URL}/product/delete`})
+            console.log(deleteProduct)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+    const data={
+        id:userId,
+        category:"supplier"
     }
 
-    const handleEdit = () =>{
-        console.log('Edit')
-    }
-    const handleDelete = () =>{
-        console.log('Delete')
-    }
+    useEffect(() => {
+        const fetchData = async() => {
+            try {
+                // Fetching all the products
+                const allProduct = await sendRequest({method:"post" ,data:data ,route:`${process.env.REACT_APP_BASE_URL}/product/all`})
+                setAllProducts(allProduct[0].products)
+                // Getting the main category id
+                const category_id = await sendRequest({method:"post" ,data:data ,route:`${process.env.REACT_APP_BASE_URL}/category/all`})
+                setCategoryId(category_id.category[0]._id)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        fetchData()
+    }, [])  
 
-    const handleSend = () =>{
-        console.log('Send')
-    }
 
   return (
     <div className='flex'>
         <SideBar/>
         <div className='flex flex-col justify-between mt-[90px] mb-[60px] mx-[100px]'>
-            <Title title={"Expiry page"}/>
+            <Title title={"Product"}/>
             <div className='flex flex-col justify-between w-[900px] h-[420px]'>
-                <div className='flex justify-between w-full'>
-                    <InputField placeholder={"Product Name"} value={name} onChange={handleChange}/>
-
+                <div className='flex justify-evenly w-full'>
+                    <InputField placeholder={"Product Name"} value={selectedRow}/>
+                    
                     <Popup contentStyle={{background: '#3AA346', width: '500px', height:'430px', borderRadius: '10px'}} position='center center' overlayStyle={{background: 'rgba(0,0,0,0.5)' }} modal={true} trigger={<Button bgColor="#3AA346" width="170px" height="46px" name={"ADD"}/>} >
                     {close => (
                         <div>
@@ -82,13 +99,16 @@ const Expiry = () => {
                         </div>
                     )}
                     </Popup>
-                    
+
                     <Button bgColor="#FF0000" width="170px" height="46px" opacity="0.7" name={"DELETE"} onClick={handleDelete}/>
-                    <Button bgColor="#3AA346" width="170px" height="46px" name={"SEND"} onClick={handleSend}/>
                 </div> 
                 <div>
-                    <TableHeader column1={"Product photo"} column2={"Product name"} column3={"Expiry date"} />
-                    <div className='h-[300px] bg-[#D9D9D9] overflow-auto'></div>
+                    <TableHeader column1={"Product name"} column2={"Product quantity"} column3={"Expiry date"} />
+                    <div className='h-[300px] bg-[#D9D9D9] overflow-auto'>
+                        {allProducts.map((item)=>{
+                            return(<ExpiryRow key={item._id}  productName={item.product_name} productQuantity={item.quantity_shelf} productExpiry={item.expiry_date} onClick={()=>setSelectedRow(item.product_name)}/>)
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
